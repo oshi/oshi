434177bf6a446bcf0c3293b518378d81c0cb9a21
dbwiddis <widdis@gmail.com>
2015-05-17 17:43:36 -0700

    Include inactive memory in mac/linux available

--- src/main/java/oshi/software/os/linux/proc/GlobalMemory.java
+++ src/main/java/oshi/software/os/linux/proc/GlobalMemory.java
@@ -23,8 +23,8 @@ public class GlobalMemory implements Memory {
 	private long totalMemory = 0;
 
 	public long getAvailable() {
-		long returnCurrentUsageMemory=0;
-		Scanner in=null;
+		long returnCurrentUsageMemory = 0;
+		Scanner in = null;
 		try {
 			in = new Scanner(new FileReader("/proc/meminfo"));
 		} catch (FileNotFoundException e) {
@@ -33,15 +33,16 @@ public class GlobalMemory implements Memory {
 		in.useDelimiter("\n");
 		while (in.hasNext()) {
 			String checkLine = in.next();
-			if (checkLine.startsWith("MemFree:") || checkLine.startsWith("MemAvailable:")) {
+			if (checkLine.startsWith("MemAvailable:")) {
 				String[] memorySplit = checkLine.split("\\s+");
-				returnCurrentUsageMemory=new Long(memorySplit[1]);
-				if (memorySplit[2].equals("kB")) {
-					returnCurrentUsageMemory*=1024;
-				}
-				if (memorySplit[0].equals("MemAvailable:")) {
-					break;
-				}
+				returnCurrentUsageMemory = calcKB(memorySplit);
+				break;
+			} else if (checkLine.startsWith("MemFree:")) {
+				String[] memorySplit = checkLine.split("\\s+");
+				returnCurrentUsageMemory += calcKB(memorySplit);
+			} else if (checkLine.startsWith("Inactive:")) {
+				String[] memorySplit = checkLine.split("\\s+");
+				returnCurrentUsageMemory += calcKB(memorySplit);
 			}
 		}
 		in.close();
@@ -50,7 +51,7 @@ public class GlobalMemory implements Memory {
 
 	public long getTotal() {
 		if (totalMemory == 0) {
-			Scanner in=null;
+			Scanner in = null;
 			try {
 				in = new Scanner(new FileReader("/proc/meminfo"));
 			} catch (FileNotFoundException e) {
@@ -62,10 +63,7 @@ public class GlobalMemory implements Memory {
 				String checkLine = in.next();
 				if (checkLine.startsWith("MemTotal:")) {
 					String[] memorySplit = checkLine.split("\\s+");
-					totalMemory=new Long(memorySplit[1]);
-					if (memorySplit[2].equals("kB")) {
-						totalMemory*=1024;
-					}
+					totalMemory = calcKB(memorySplit);
 					break;
 				}
 			}
@@ -73,4 +71,13 @@ public class GlobalMemory implements Memory {
 		}
 		return totalMemory;
 	}
+
+	private long calcKB(String[] memorySplit) {
+		long memory = new Long(memorySplit[1]);
+		if (memorySplit[2].equals("kB")) {
+			memory *= 1024;
+		}
+		return memory;
+	}
+
 }
--- src/main/java/oshi/software/os/mac/local/GlobalMemory.java
+++ src/main/java/oshi/software/os/mac/local/GlobalMemory.java
@@ -20,23 +20,30 @@ public class GlobalMemory implements Memory {
 	private long totalMemory = 0;
 
 	public long getAvailable() {
-		long returnCurrentUsageMemory=0;
+		long returnCurrentUsageMemory = 0;
 		for (String line : ExecutingCommand.runNative("vm_stat")) {
 			if (line.startsWith("Pages free:")) {
 				String[] memorySplit = line.split(":\\s+");
-				returnCurrentUsageMemory+=new Long(memorySplit[1].replace(".",""));
-			} else if(line.startsWith("Pages speculative:")) {
+				returnCurrentUsageMemory += new Long(memorySplit[1].replace(
+						".", ""));
+			} else if (line.startsWith("Pages speculative:")) {
 				String[] memorySplit = line.split(":\\s+");
-				returnCurrentUsageMemory+=new Long(memorySplit[1].replace(".",""));
+				returnCurrentUsageMemory += new Long(memorySplit[1].replace(
+						".", ""));
+			} else if (line.startsWith("Pages inactive:")) {
+				String[] memorySplit = line.split(":\\s+");
+				returnCurrentUsageMemory += new Long(memorySplit[1].replace(
+						".", ""));
 			}
 		}
-		returnCurrentUsageMemory=returnCurrentUsageMemory*4096;
+		returnCurrentUsageMemory = returnCurrentUsageMemory * 4096;
 		return returnCurrentUsageMemory;
 	}
 
 	public long getTotal() {
 		if (totalMemory == 0) {
-			totalMemory=new Long(ExecutingCommand.getFirstAnswer("sysctl -n hw.memsize"));
+			totalMemory = new Long(
+					ExecutingCommand.getFirstAnswer("sysctl -n hw.memsize"));
 		}
 		return totalMemory;
 	}
