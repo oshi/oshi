0babcdc7b9175d161bf447f2b4d687056ab059a8
Laurent RICHARD <laurent-richard@harvest.fr>
2015-08-21 16:46:29 +0200

    add new versions of Windows (10)
    Windows 10 & Windows Server 2016 (technical preview)

--- src/main/java/oshi/software/os/windows/nt/OSVersionInfoEx.java
+++ src/main/java/oshi/software/os/windows/nt/OSVersionInfoEx.java
@@ -16,8 +16,6 @@
  */
 package oshi.software.os.windows.nt;
 
-import oshi.software.os.OperatingSystemVersion;
-
 import com.sun.jna.Native;
 import com.sun.jna.platform.win32.Advapi32Util;
 import com.sun.jna.platform.win32.Kernel32;
@@ -27,6 +25,8 @@ import com.sun.jna.platform.win32.WinNT;
 import com.sun.jna.platform.win32.WinNT.OSVERSIONINFOEX;
 import com.sun.jna.platform.win32.WinReg;
 import com.sun.jna.platform.win32.WinUser;
+
+import oshi.software.os.OperatingSystemVersion;
 
 /**
  * Contains operating system version information. The information includes major
@@ -124,73 +124,44 @@ public class OSVersionInfoEx implements OperatingSystemVersion {
 	public String toString() {
 		String version = null;
 
-		// see
-		// http://msdn.microsoft.com/en-us/library/windows/desktop/ms724833%28v=vs.85%29.aspx
-		if (getPlatformId() == WinNT.VER_PLATFORM_WIN32_NT) {
-			// 8.1
-			if (getMajor() == 6 && getMinor() == 3 && getProductType() == WinNT.VER_NT_WORKSTATION) {
-				version = "8.1";
-			}
-			// Server 2008 R2
-			else if (getMajor() == 6 && getMinor() == 3 && getProductType() != WinNT.VER_NT_WORKSTATION) {
-				version = "Server 2012 R2";
-			}
-			// 8
-			else if (getMajor() == 6 && getMinor() == 2 && getProductType() == WinNT.VER_NT_WORKSTATION) {
-				version = "8";
-			}
-			// Server 2008
-			else if (getMajor() == 6 && getMinor() == 2 && getProductType() != WinNT.VER_NT_WORKSTATION) {
-				version = "Server 2012";
-			}
-			// 7
-			else if (getMajor() == 6 && getMinor() == 1 && getProductType() == WinNT.VER_NT_WORKSTATION) {
-				version = "7";
-			}
-			// Server 2008 R2
-			else if (getMajor() == 6 && getMinor() == 1 && getProductType() != WinNT.VER_NT_WORKSTATION) {
-				version = "Server 2008 R2";
-			}
-			// Server 2008
-			else if (getMajor() == 6 && getMinor() == 0 && getProductType() != WinNT.VER_NT_WORKSTATION) {
-				version = "Server 2008";
-			}
-			// Vista
-			else if (getMajor() == 6 && getMinor() == 0 && getProductType() == WinNT.VER_NT_WORKSTATION) {
-				version = "Vista";
-			}
-			// Server 2003
-			else if (getMajor() == 5 && getMinor() == 2 && getProductType() != WinNT.VER_NT_WORKSTATION
-					&& User32.INSTANCE.GetSystemMetrics(WinUser.SM_SERVERR2) != 0) {
-				version = "Server 2003";
-			}
-			// Server 2003 R2
-			else if (getMajor() == 5 && getMinor() == 2 && getProductType() != WinNT.VER_NT_WORKSTATION
-					&& User32.INSTANCE.GetSystemMetrics(WinUser.SM_SERVERR2) == 0) {
-				version = "Server 2003 R2";
-			}
-			// XP 64 bit
-			else if (getMajor() == 5 && getMinor() == 2 && getProductType() == WinNT.VER_NT_WORKSTATION) {
-				version = "XP";
-			}
-			// XP 32 bit
-			else if (getMajor() == 5 && getMinor() == 1) {
-				version = "XP";
-			}
-			// 2000
-			else if (getMajor() == 5 && getMinor() == 0) {
-				version = "2000";
-			}
-			// Windows NT
-			else if (getMajor() == 4) {
-				version = "NT 4";
-
-				if ("Service Pack 6".equals(getServicePack())) {
-					if (Advapi32Util.registryKeyExists(WinReg.HKEY_LOCAL_MACHINE,
-							"SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Hotfix\\Q246009")) {
-						return "NT4 SP6a";
-					}
-				}
+        // see http://msdn.microsoft.com/en-us/library/windows/desktop/ms724833%28v=vs.85%29.aspx
+        if (getPlatformId() == WinNT.VER_PLATFORM_WIN32_NT) {
+            boolean ntWorkstation = getProductType() == WinNT.VER_NT_WORKSTATION;
+            if (getMajor() == 10 && getMinor() == 0 && ntWorkstation) {
+                version = "10";
+            } else if (getMajor() == 10 && getMinor() == 0 && !ntWorkstation) {
+                version = "Server 2016";
+            } else if (getMajor() == 6 && getMinor() == 3 && ntWorkstation) {
+                version = "8.1";
+            } else if (getMajor() == 6 && getMinor() == 3 && !ntWorkstation) {
+                version = "Server 2012 R2";
+            } else if (getMajor() == 6 && getMinor() == 2 && ntWorkstation) {
+                version = "8";
+            } else if (getMajor() == 6 && getMinor() == 2 && !ntWorkstation) {
+                version = "Server 2012";
+            } else if (getMajor() == 6 && getMinor() == 1 && ntWorkstation) {
+                version = "7";
+            } else if (getMajor() == 6 && getMinor() == 1 && !ntWorkstation) {
+                version = "Server 2008 R2";
+            } else if (getMajor() == 6 && getMinor() == 0 && !ntWorkstation) {
+                version = "Server 2008";
+            } else if (getMajor() == 6 && getMinor() == 0 && ntWorkstation) {
+                version = "Vista";
+            } else if (getMajor() == 5 && getMinor() == 2 && !ntWorkstation) {
+                version = User32.INSTANCE.GetSystemMetrics(WinUser.SM_SERVERR2) != 0 ? "Server 2003" : "Server 2003 R2";
+            } else if (getMajor() == 5 && getMinor() == 2 && ntWorkstation) {
+                version = "XP"; // 64 bits
+            } else if (getMajor() == 5 && getMinor() == 1) {
+                version = "XP"; // 32 bits
+            } else if (getMajor() == 5 && getMinor() == 0) {
+                version = "2000";
+            } else if (getMajor() == 4) {
+                version = "NT 4";
+                if ("Service Pack 6".equals(getServicePack())) {
+                    if (Advapi32Util.registryKeyExists(WinReg.HKEY_LOCAL_MACHINE, "SOFTWARE\\Microsoft\\Windows NT\\CurrentVersion\\Hotfix\\Q246009")) {
+                        version = "NT4 SP6a";
+                    }
+                }
 
 			} else {
 				throw new RuntimeException("Unsupported Windows NT version: " + this._versionInfo.toString());
