6f36cd2c3b12bd8493495f303f1b4feaa1a6acd6
Enrico Bianchi <enrico.bianchi@gmail.com>
2016-04-20 16:58:25 +0200

    Implemented Windows swap total and Windows swap use methods

--- src/main/java/oshi/hardware/platform/windows/WindowsGlobalMemory.java
+++ src/main/java/oshi/hardware/platform/windows/WindowsGlobalMemory.java
@@ -1,13 +1,13 @@
 /**
  * Oshi (https://github.com/dblock/oshi)
- * 
+ *
  * Copyright (c) 2010 - 2016 The Oshi Project Team
- * 
+ *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  * dblock[at]dblock[dot]org
  * alessandro[at]perucchi[dot]org
@@ -32,7 +32,7 @@ import oshi.json.NullAwareJsonObjectBuilder;
 
 /**
  * Memory obtained by GlobalMemoryStatusEx.
- * 
+ *
  * @author dblock[at]dblock[dot]org
  */
 public class WindowsGlobalMemory implements GlobalMemory {
@@ -70,16 +70,36 @@ public class WindowsGlobalMemory implements GlobalMemory {
 
     @Override
     public long getSwapTotal() {
-        // TODO: for now, it returns 0
-        return 0;
+        // NOTE: other ways to get the Windows page file size are:
+        // 1 - The Win32_PageFile WMI class via OLE32 call;
+        // 2 - The EnumPageFiles via Psapi call. 
+
+        if (!Kernel32.INSTANCE.GlobalMemoryStatusEx(this._memory)) {
+            LOG.error("Failed to Initialize MemoryStatusEx. Error code: {}", Kernel32.INSTANCE.GetLastError());
+            this._memory = null;
+            return 0L;
+        }
+
+        return this._memory.ullTotalPageFile.longValue() - this._memory.ullTotalPhys.longValue();
     }
 
     @Override
     public long getSwapUsed() {
-        // TODO: for now, it returns 0
-        return 0;
+        long total;
+        long available;
+        
+        if (!Kernel32.INSTANCE.GlobalMemoryStatusEx(this._memory)) {
+            LOG.error("Failed to Initialize MemoryStatusEx. Error code: {}", Kernel32.INSTANCE.GetLastError());
+            this._memory = null;
+            return 0L;
+        }
+
+        total = this.getSwapTotal();
+        available = this._memory.ullAvailPageFile.longValue() - this._memory.ullAvailPhys.longValue();
+        
+        return total - available;
     }
-    
+
     @Override
     public JsonObject toJSON() {
         return NullAwareJsonObjectBuilder.wrap(jsonFactory.createObjectBuilder()).add("available", getAvailable()).add("total", getTotal()).build();
