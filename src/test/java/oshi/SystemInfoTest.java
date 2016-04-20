fbdf5a452f3856d696f497687635121ba557d6d5
Enrico Bianchi <enrico.bianchi@gmail.com>
2016-04-20 10:26:14 +0200

    Fixed unit test

--- src/test/java/oshi/SystemInfoTest.java
+++ src/test/java/oshi/SystemInfoTest.java
@@ -1,13 +1,13 @@
 /**
  * Oshi (https://github.com/dblock/oshi)
- * 
+ *
  * Copyright (c) 2010 - 2016 The Oshi Project Team
- * 
+ *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * 
+ *
  * Contributors:
  * dblock[at]dblock[dot]org
  * alessandro[at]perucchi[dot]org
@@ -92,8 +92,8 @@ public class SystemInfoTest {
         
         // Swap tests
         assertTrue(memory.getSwapTotal() >= 0);
-        assertTrue(memory.getSwapAvailable()>= 0);
-        assertTrue(memory.getSwapAvailable() <= memory.getSwapTotal());
+        assertTrue(memory.getSwapUsed() >= 0);
+        assertTrue(memory.getSwapUsed() <= memory.getSwapTotal());
     }
 
     /**
