8d7f1675482168b1bad1449b5b12f5374574c527
Enrico Bianchi <enrico.bianchi@gmail.com>
2016-04-21 16:09:15 +0200

    Added interface for getting disks information

---
+++ src/main/java/oshi/hardware/Disks.java
@@ -0,0 +1,24 @@
+/*
+ * Copyright (c) 2016 com.github.dblock.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * enrico[dot]bianchi[at]gmail[dot]com
+ *    com.github.dblock - initial API and implementation and/or initial documentation
+ */
+package oshi.hardware;
+
+import oshi.json.OshiJsonObject;
+
+/**
+ * Disks refers to hard drives installed in the machine.
+ *
+ * @author enrico[dot]bianchi[at]gmail[dot]com
+ */
+public interface Disks extends OshiJsonObject {
+
+    public HWDiskStore[] getDisks();
+}
