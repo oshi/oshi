70f357b52dcb470ff7f4800c8076611c43ff3140
Enrico Bianchi <enrico.bianchi@gmail.com>
2016-04-30 01:22:34 +0200

    Added per platform network interfaces extraction classes

---
+++ src/main/java/oshi/hardware/platform/linux/LinuxNetworks.java
@@ -0,0 +1,32 @@
+/*
+ * Copyright (c) 2016 com.github.dblock.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * enrico[dot]bianchi[at]gmail[dot]com
+ *    com.github.dblock - initial API and implementation and/or initial documentation
+ */
+package oshi.hardware.platform.linux;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import oshi.hardware.common.AbstractNetworks;
+import oshi.hardware.stores.HWNetworkStore;
+
+/**
+ *
+ * @author enrico[dot]bianchi[at]gmail[dot]com
+ */
+public class LinuxNetworks extends AbstractNetworks {
+
+    private static final Logger LOG = LoggerFactory.getLogger(LinuxNetworks.class);
+
+    @Override
+    public HWNetworkStore[] getNetworks() {
+        // TODO: implement network interfaces extraction
+        return new HWNetworkStore[]{};
+    }
+}
---
+++ src/main/java/oshi/hardware/platform/mac/MacNetworks.java
@@ -0,0 +1,28 @@
+/*
+ * Copyright (c) 2016 com.github.dblock.
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Contributors:
+ * enrico[dot]bianchi[at]gmail[dot]com
+ *    com.github.dblock - initial API and implementation and/or initial documentation
+ */
+package oshi.hardware.platform.mac;
+
+import oshi.hardware.common.AbstractNetworks;
+import oshi.hardware.stores.HWNetworkStore;
+
+/**
+ *
+ * @author enrico[dot]bianchi[at]gmail[dot]com
+ */
+public class MacNetworks extends AbstractNetworks {
+
+@Override
+    public HWNetworkStore[] getNetworks() {
+        // TODO: implement network interfaces extraction
+        return new HWNetworkStore[]{};
+    }
+}
