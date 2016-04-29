c41720258c5af8877f2ffe9a7ec6ecf24c986420
Enrico Bianchi <enrico.bianchi@gmail.com>
2016-04-30 01:54:53 +0200

    Added code for retrieving network interfaces information on Windows

--- src/main/java/oshi/hardware/platform/windows/WindowsNetworks.java
+++ src/main/java/oshi/hardware/platform/windows/WindowsNetworks.java
@@ -11,6 +11,14 @@
  */
 package oshi.hardware.platform.windows;
 
+import java.net.NetworkInterface;
+import java.net.SocketException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Enumeration;
+import java.util.List;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import oshi.hardware.common.AbstractNetworks;
 import oshi.hardware.stores.HWNetworkStore;
 
@@ -20,10 +28,36 @@ import oshi.hardware.stores.HWNetworkStore;
  */
 public class WindowsNetworks extends AbstractNetworks {
 
-    @Override
-    public HWNetworkStore[] getNetworks() {
-        // TODO: implement network interfaces extraction
-        return new HWNetworkStore[]{};
+    private static final Logger LOG = LoggerFactory.getLogger(WindowsNetworks.class);
+
+    private void setNetworkStats(HWNetworkStore netstore) {
+        // TODO: set network stats on Windows
     }
 
+    @Override
+    public HWNetworkStore[] getNetworks() {
+        Enumeration<NetworkInterface> interfaces;
+        HWNetworkStore netstore;
+        List<HWNetworkStore> result;
+        StringBuilder sb;
+        byte[] mac;
+
+        result = new ArrayList<>();
+
+        try {
+            interfaces = NetworkInterface.getNetworkInterfaces();
+            for (NetworkInterface netint : Collections.list(interfaces)) {
+                if (!netint.getDisplayName().equals("lo")) {
+                    netstore = new HWNetworkStore();
+                    this.setNetworkParameters(netstore, netint);
+                    this.setNetworkStats(netstore);
+                    result.add(netstore);
+                }
+            }
+        } catch (SocketException ex) {
+            LOG.debug("Socket exception when retrieving network interfaces: " + ex.getMessage());
+        }
+
+        return result.toArray(new HWNetworkStore[result.size()]);
+    }
 }
