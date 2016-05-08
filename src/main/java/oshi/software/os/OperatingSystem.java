8ec76c4c1dc9c3d1793ab4a082755bbe455f3741
Corey Holland <cholland1989@me.com>
2016-05-08 11:23:21 -0500

    Add methods to retrieve system-wide file descriptors

--- src/main/java/oshi/software/os/OperatingSystem.java
+++ src/main/java/oshi/software/os/OperatingSystem.java
@@ -48,4 +48,18 @@ public interface OperatingSystem extends OshiJsonObject {
      * @return Version.
      */
     OperatingSystemVersion getVersion();
+
+    /**
+     * Open file descriptors.
+     *
+     * @return long
+     */
+    long getOpenDescriptors();
+
+    /**
+     * Maximum file descriptors.
+     *
+     * @return long
+     */
+    long getMaxDescriptors();
 }
