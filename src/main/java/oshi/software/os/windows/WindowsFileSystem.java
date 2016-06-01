70ea4d6ee6dd4d69707794a8809102cc6853a43b
Enrico Bianchi <enrico.bianchi@gmail.com>
2016-06-01 11:50:17 +0200

    Splitted pulic method in two private methods

--- src/main/java/oshi/software/os/windows/WindowsFileSystem.java
+++ src/main/java/oshi/software/os/windows/WindowsFileSystem.java
@@ -18,10 +18,10 @@
  */
 package oshi.software.os.windows;
 
-import com.sun.jna.platform.win32.WinDef;
 import com.sun.jna.platform.win32.WinNT;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Map;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -29,6 +29,7 @@ import org.slf4j.LoggerFactory;
 import oshi.jna.platform.windows.Kernel32;
 import oshi.software.common.AbstractFileSystem;
 import oshi.software.os.OSFileStore;
+import oshi.util.platform.windows.WmiUtil;
 
 /**
  * The Windows File System contains {@link OSFileStore}s which are a storage
@@ -43,6 +44,7 @@ public class WindowsFileSystem extends AbstractFileSystem {
     private static final long serialVersionUID = 1L;
 
     private static final Logger LOG = LoggerFactory.getLogger(WindowsFileSystem.class);
+    private final int BUFSIZE = 255;
 
     public WindowsFileSystem() {
         // Set error mode to fail rather than prompt for FLoppy/CD-Rom
@@ -57,41 +59,63 @@ public class WindowsFileSystem extends AbstractFileSystem {
      *         {@link OSFileStore#getTotalSpace()} = 0.
      */
     public OSFileStore[] getFileStores() {
-        List<OSFileStore> fs = new ArrayList<>();
-        final int LENGTH = 255;
-        char[] aVolume = new char[LENGTH];
+        List<OSFileStore> locals, networks;
+        ArrayList<OSFileStore> result;
 
-        WinNT.HANDLE hVol = Kernel32.INSTANCE.FindFirstVolume(aVolume, LENGTH);
+        result = new ArrayList<>();
+        locals = this.getLocalVolumes();
+        networks = this.getNetworkVolumes();
+
+        result.addAll(locals);
+        result.addAll(networks);
+
+        return result.toArray(new OSFileStore[result.size()]);
+    }
+
+    private List<OSFileStore> getLocalVolumes() {
+        List<OSFileStore> fs = new ArrayList<>();
+        char[] aVolume = new char[BUFSIZE];
+
+        WinNT.HANDLE hVol = Kernel32.INSTANCE.FindFirstVolume(aVolume, BUFSIZE);
         if (hVol == WinNT.INVALID_HANDLE_VALUE) {
-            return fs.toArray(new OSFileStore[0]);
+            return fs;
         }
 
         while (true) {
             char[] fstype = new char[16];
-            char[] name = new char[LENGTH];
-            char[] mount = new char[LENGTH];
+            char[] name = new char[BUFSIZE];
+            char[] mount = new char[BUFSIZE];
 
             WinNT.LARGE_INTEGER userFreeBytes = new WinNT.LARGE_INTEGER(0L);
             WinNT.LARGE_INTEGER totalBytes = new WinNT.LARGE_INTEGER(0L);
             WinNT.LARGE_INTEGER systemFreeBytes = new WinNT.LARGE_INTEGER(0L);
-        
+
             String volume = new String(aVolume).trim();
-            Kernel32.INSTANCE.GetVolumeInformation(volume, name, LENGTH, null, null, null, fstype, 16);
-            Kernel32.INSTANCE.GetVolumePathNamesForVolumeName(volume, mount, LENGTH, null);
+            Kernel32.INSTANCE.GetVolumeInformation(volume, name, BUFSIZE, null, null, null, fstype, 16);
+            Kernel32.INSTANCE.GetVolumePathNamesForVolumeName(volume, mount, BUFSIZE, null);
             Kernel32.INSTANCE.GetDiskFreeSpaceEx(volume, userFreeBytes, totalBytes, systemFreeBytes);
 
             fs.add(new OSFileStore(volume, new String(mount).trim(),
                     new String(name).trim(), new String(fstype).trim(),
                     systemFreeBytes.getValue(), totalBytes.getValue()));
 
-            boolean retVal = Kernel32.INSTANCE.FindNextVolume(hVol, aVolume, LENGTH);
+            boolean retVal = Kernel32.INSTANCE.FindNextVolume(hVol, aVolume, BUFSIZE);
             if (!retVal) {
                 Kernel32.INSTANCE.FindVolumeClose(hVol);
                 break;
             }
         }
 
-        return fs.toArray(new OSFileStore[fs.size()]);
+        return fs;
+    }
+
+    private List<OSFileStore> getNetworkVolumes() {
+        List<OSFileStore> fs = new ArrayList<>();
+
+        Map<String, List<String>> drives = WmiUtil.selectStringsFrom(null, "Win32_LogicalDisk",
+                "Name,Description,ProviderName,FileSystem,Freespace,Size", "WHERE DriveType = 4");
+
+        return fs;
     }
 
     @Override
