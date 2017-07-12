91b9e696048f9602a77727d55978a5f5f3c02250
Daniel Widdis <widdis@gmail.com>
2017-07-11 22:08:53 -0700

    Update dependencies

--- oshi-core/src/main/java/oshi/hardware/platform/linux/LinuxCentralProcessor.java
+++ oshi-core/src/main/java/oshi/hardware/platform/linux/LinuxCentralProcessor.java
@@ -1,18 +1,18 @@
 /**
  * Oshi (https://github.com/oshi/oshi)
- * <p>
+ *
  * Copyright (c) 2010 - 2017 The Oshi Project Team
- * <p>
+ *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
  * which accompanies this distribution, and is available at
  * http://www.eclipse.org/legal/epl-v10.html
- * <p>
+ *
  * Maintainers:
  * dblock[at]dblock[dot]org
  * widdis[at]gmail[dot]com
  * enrico.bianchi[at]gmail[dot]com
- * <p>
+ *
  * Contributors:
  * https://github.com/oshi/oshi/graphs/contributors
  */
