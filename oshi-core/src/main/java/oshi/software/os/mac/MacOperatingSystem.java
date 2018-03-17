cbeb349cae8a3db6b01aff099092b9e3500e05e1
Alex O'Ree <alexoree@apache.org>
2018-03-17 10:22:17 -0400

    #459 pr issues

--- oshi-core/src/main/java/oshi/software/os/mac/MacOperatingSystem.java
+++ oshi-core/src/main/java/oshi/software/os/mac/MacOperatingSystem.java
@@ -29,8 +29,6 @@ import com.sun.jna.Memory;
 import com.sun.jna.Native;
 import com.sun.jna.Pointer;
 import com.sun.jna.ptr.IntByReference;
-import java.util.Collection;
-import java.util.LinkedList;
 
 import oshi.jna.platform.mac.SystemB;
 import oshi.jna.platform.mac.SystemB.Group;
