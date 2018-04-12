59c1f264de1528fb8bb79d838e928b1f5b1595bb
Daniel Widdis <widdis@gmail.com>
2018-04-11 23:19:18 -0700

    Update License for 2018

--- oshi-json/src/main/java/oshi/json/software/os/OperatingSystem.java
+++ oshi-json/src/main/java/oshi/json/software/os/OperatingSystem.java
@@ -1,7 +1,7 @@
 /**
  * Oshi (https://github.com/oshi/oshi)
  *
- * Copyright (c) 2010 - 2017 The Oshi Project Team
+ * Copyright (c) 2010 - 2018 The Oshi Project Team
  *
  * All rights reserved. This program and the accompanying materials
  * are made available under the terms of the Eclipse Public License v1.0
