1c0b3cdc1655d09ad9ff25ed3350493d744b5245
Daniel Widdis <widdis@gmail.com>
2018-04-11 22:11:21 -0700

    Include sources and javadocs with shaded jar

---
+++ oshi-core-shaded/src/main/java/oshi/package-info.java
@@ -0,0 +1,22 @@
+/**
+ * Oshi (https://github.com/oshi/oshi)
+ *
+ * Copyright (c) 2010 - 2017 The Oshi Project Team
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * which accompanies this distribution, and is available at
+ * http://www.eclipse.org/legal/epl-v10.html
+ *
+ * Maintainers:
+ * dblock[at]dblock[dot]org
+ * widdis[at]gmail[dot]com
+ * enrico.bianchi[at]gmail[dot]com
+ *
+ * Contributors:
+ * https://github.com/oshi/oshi/graphs/contributors
+ */
+/**
+ * [oshi-core-shaded] See the oshi-core javadoc for documentation.
+ */
+package oshi;
