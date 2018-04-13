2c4deeacba9beed17b79bb97879ee80528979bfd
Daniel Widdis <widdis@gmail.com>
2018-04-12 20:22:18 -0700

    OS Tests for Child Processes

--- oshi-json/src/test/java/oshi/json/software/os/OperatingSystemTest.java
+++ oshi-json/src/test/java/oshi/json/software/os/OperatingSystemTest.java
@@ -22,6 +22,12 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 import org.junit.Test;
 
 import oshi.json.SystemInfo;
@@ -73,6 +79,107 @@ public class OperatingSystemTest {
         assertTrue(proc.getStartTime() >= 0);
         assertTrue(proc.getBytesRead() >= 0);
         assertTrue(proc.getBytesWritten() >= 0);
+    }
+
+    /**
+     * Tests process query by pid list
+     */
+    @Test
+    public void testProcessQueryByList() {
+        SystemInfo si = new SystemInfo();
+        OperatingSystem os = si.getOperatingSystem();
+        assertNotNull(os.getFamily());
+        assertNotNull(os.getManufacturer());
+        OperatingSystemVersion version = os.getVersion();
+        assertNotNull(version);
+        assertNotNull(version.getVersion());
+        assertNotNull(version.getCodeName());
+        assertNotNull(version.getBuildNumber());
+
+        assertTrue(os.getProcessCount() >= 1);
+        assertTrue(os.getThreadCount() >= 1);
+        assertTrue(os.getProcessId() > 0);
+
+        OSProcess[] processes = os.getProcesses(5, null);
+        assertNotNull(processes);
+        // every OS should have at least one process running on it
+        assertTrue(processes.length > 0);
+        // the list of pids we want info on
+        List<Integer> pids = new ArrayList<>();
+        for (OSProcess p : processes) {
+            pids.add(p.getProcessID());
+        }
+        // query for just those processes
+        Collection<OSProcess> processes1 = os.getProcesses(pids);
+        // theres a potential for a race condition here, if a process we queried
+        // for initially wasn't running during the second query. In this case,
+        // try again with the shorter list
+        while (processes1.size() < pids.size()) {
+            pids.clear();
+            for (OSProcess p : processes1) {
+                pids.add(p.getProcessID());
+            }
+            // query for just those processes
+            processes1 = os.getProcesses(pids);
+        }
+        assertEquals(processes1.size(), pids.size());
+    }
+
+    /**
+     * Tests child process getter
+     */
+    @Test
+    public void testGetChildProcesses() {
+        // Get list of PIDS
+        SystemInfo si = new SystemInfo();
+        OperatingSystem os = si.getOperatingSystem();
+        OSProcess[] processes = os.getProcesses(0, null);
+        Map<Integer, Integer> childMap = new HashMap<>();
+        // First iteration to set all 0's
+        for (OSProcess p : processes) {
+            childMap.put(p.getProcessID(), 0);
+            childMap.put(p.getParentProcessID(), 0);
+        }
+        // Second iteration to count children
+        for (OSProcess p : processes) {
+            childMap.put(p.getParentProcessID(), childMap.get(p.getParentProcessID()) + 1);
+        }
+        // Find a PID with 0, 1, and N>1 children
+        int zeroPid = -1;
+        int onePid = -1;
+        int nPid = -1;
+        int nNum = 0;
+        int mPid = -1;
+        int mNum = 0;
+        for (Integer i : childMap.keySet()) {
+            if (zeroPid < 0 && childMap.get(i) == 0) {
+                zeroPid = i;
+            } else if (onePid < 0 && childMap.get(i) == 1) {
+                onePid = i;
+            } else if (nPid < 0 && childMap.get(i) > 1) {
+                nPid = i;
+                nNum = childMap.get(i);
+            } else if (mPid < 0 && childMap.get(i) > 1) {
+                mPid = i;
+                mNum = childMap.get(i);
+            }
+            if (zeroPid >= 0 && onePid >= 0 && nPid >= 0 && mPid >= 0) {
+                break;
+            }
+        }
+        if (zeroPid >= 0) {
+            assertEquals(os.getChildProcesses(zeroPid, 0, null).length, 0);
+        }
+        if (onePid >= 0) {
+            assertEquals(os.getChildProcesses(onePid, 0, null).length, 1);
+        }
+        // Due to race condition, a process may terminate before we count its
+        // children. nPid is probably PID=1 with all PIDs with no other parent
+        // In case one has ended we'll try a backup
+        if (nPid >= 0 && mPid >= 0) {
+            assertTrue(os.getChildProcesses(nPid, 0, null).length == nNum
+                    || os.getChildProcesses(mPid, 0, null).length == mNum);
+        }
     }
 
     /**
