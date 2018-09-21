b6f74419defc1f551ec744f76317ab4c3c939d38
BilalAM <bilal.asif.97@gmail.com>
2018-09-21 20:01:27 +0500

    Sound card interface

---
+++ oshi-core/src/main/java/oshi/hardware/SoundCard.java
@@ -0,0 +1,32 @@
+package oshi.hardware;
+
+public interface SoundCard {
+
+
+    /**
+     * Retrieves the driver version currently in use in machine
+     * @return The current and complete name of the driver version
+     */
+    String getDriverVersion();
+
+
+    /**
+     * Retrieves the full name of the card.
+     * @return The name of the card.
+     */
+    String getName();
+
+
+    /**
+     * Retrieves the codec of the Sound card
+     * @return The name of the codec of the sound card
+     */
+    String getCodec();
+
+    /**
+     * Retrieves all the sound cards currently contained in our machine
+     * @return
+     */
+    SoundCard[] getDevices();
+
+}
