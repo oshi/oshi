ac82c3a5ae12330d602345086d534b37ed2806e8
BilalAM <bilal.asif.97@gmail.com>
2018-10-01 00:40:49 +0500

    SoundCard name and driver is done

--- oshi-core/src/main/java/oshi/hardware/platform/windows/WindowsSoundCard.java
+++ oshi-core/src/main/java/oshi/hardware/platform/windows/WindowsSoundCard.java
@@ -1,5 +1,6 @@
 package oshi.hardware.platform.windows;
 
+import oshi.hardware.SoundCard;
 import oshi.hardware.common.AbstractSoundCard;
 import oshi.jna.platform.windows.WbemcliUtil;
 import oshi.util.platform.windows.WmiUtil;
@@ -16,31 +17,61 @@ public class WindowsSoundCard extends AbstractSoundCard {
         MANUFACTURER, NAME
     }
 
-    private static final String AUDIO_CARD = "Win32_SoundDevice";
-    private static final WbemcliUtil.WmiQuery<AudioCardName> AUDIO_CARD_QUERY =
-            new WbemcliUtil.WmiQuery<>(AUDIO_CARD, AudioCardName.class);
-    private static final WbemcliUtil.WmiResult<AudioCardName> AUDIO_CARD_QUERY_RESULT = WmiUtil.queryWMI(AUDIO_CARD_QUERY);
+    private static WbemcliUtil.WmiQuery<AudioCardName> AUDIO_CARD_QUERY;
 
+    private static final String AUDIO_CARD = "Win32_SoundDevice";
+    private static WbemcliUtil.WmiQuery<AudioCardKernel> AUDIO_CARD_KERNAL_QUERY;
+    private static WbemcliUtil.WmiResult<AudioCardName> AUDIO_CARD_QUERY_RESULT;
+    private static WbemcliUtil.WmiResult<AudioCardKernel> AUDIO_CARD_KERNEL_QUERY_RESULT;
+
+    static {
+        AUDIO_CARD_QUERY =
+                new WbemcliUtil.WmiQuery<>(AUDIO_CARD, AudioCardName.class);
+        AUDIO_CARD_QUERY_RESULT = WmiUtil.queryWMI(AUDIO_CARD_QUERY);
+    }
+
+    private static String getAudioCardCompleteName(int index) {
+        return String.valueOf(AUDIO_CARD_QUERY_RESULT.getValue(AudioCardName.MANUFACTURER, 0)) + " " + String.valueOf(AUDIO_CARD_QUERY_RESULT.getValue(AudioCardName.NAME, 0));
+    }
 
     public WindowsSoundCard(String kernelVersion, String name, String codec) {
         super(kernelVersion, name, codec);
     }
 
-    private static String getAudioCardCompleteName() {
-        return String.valueOf(AUDIO_CARD_QUERY_RESULT.getValue(AudioCardName.MANUFACTURER, 0)) + " " + String.valueOf(AUDIO_CARD_QUERY_RESULT.getValue(AudioCardName.NAME, 0));
+    private static String getAudioCardKernelVersion(int index) {
+        String audioCardKernel = "";
+        if (AUDIO_CARD_QUERY_RESULT.getResultCount() == 0) {
+            // log warning.
+        } else {
+            AUDIO_CARD_KERNAL_QUERY = new WbemcliUtil.WmiQuery<>("Win32_PnPSignedDriver WHERE DeviceName=\""
+                    + AUDIO_CARD_QUERY_RESULT.getValue(AudioCardName.NAME, index)
+                    + "\"", AudioCardKernel.class);
+            AUDIO_CARD_KERNEL_QUERY_RESULT = WmiUtil.queryWMI(AUDIO_CARD_KERNAL_QUERY);
+            audioCardKernel =
+                    String.valueOf(AUDIO_CARD_KERNEL_QUERY_RESULT.getValue(AudioCardKernel.DRIVERPROVIDERNAME, index)) + " "
+                            + String.valueOf(AUDIO_CARD_KERNEL_QUERY_RESULT.getValue(AudioCardKernel.DEVICENAME, index)) + " "
+                            + String.valueOf(AUDIO_CARD_KERNEL_QUERY_RESULT.getValue(AudioCardKernel.DRIVERNAME, index)) + " "
+                            + String.valueOf(AUDIO_CARD_KERNEL_QUERY_RESULT.getValue(AudioCardKernel.DRIVERVERSION, index));
+        }
+        return audioCardKernel;
     }
 
-
-    public List<WindowsSoundCard> getSoundCards() {
+    public static List<WindowsSoundCard> getSoundCards() {
         List<WindowsSoundCard> cards = new ArrayList<>();
         for (int i = 0; i < AUDIO_CARD_QUERY_RESULT.getResultCount(); i++) {
-            cards.add(new WindowsSoundCard(null, getAudioCardCompleteName(), null));
+            cards.add(new WindowsSoundCard(getAudioCardKernelVersion(i), getAudioCardCompleteName(i), "bla bla"));
         }
         return cards;
     }
 
     public static void main(String[] args) {
-        System.out.println(getAudioCardCompleteName());
+        for (SoundCard cards : getSoundCards()) {
+            System.out.println(cards.toString());
+        }
+    }
+
+    enum AudioCardKernel {
+        DRIVERPROVIDERNAME, DRIVERNAME, DRIVERVERSION, DEVICENAME
     }
 
 }
