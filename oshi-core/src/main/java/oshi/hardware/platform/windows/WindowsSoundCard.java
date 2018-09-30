5457a11a6815602881cb1bdb2cb4e8ada070833f
Bilal Asif Mirza <bilal.asif.97@gmail.com>
2018-09-30 20:39:23 +0500

    audio card complete name

--- oshi-core/src/main/java/oshi/hardware/platform/windows/WindowsSoundCard.java
+++ oshi-core/src/main/java/oshi/hardware/platform/windows/WindowsSoundCard.java
@@ -1,12 +1,28 @@
 package oshi.hardware.platform.windows;
 
 import oshi.hardware.common.AbstractSoundCard;
+import oshi.jna.platform.windows.WbemcliUtil;
+import oshi.util.platform.windows.WmiUtil;
 
 /**
  * @author : BilalAM
  */
 public class WindowsSoundCard extends AbstractSoundCard {
-        public WindowsSoundCard(String kernelVersion, String name, String codec) {
-                super(kernelVersion, name, codec);
-        }
+
+    enum AudioCardCaption {
+        MANUFACTURER, NAME
+    }
+
+    private static final WbemcliUtil.WmiQuery<AudioCardCaption> AUDIO_CARD_QUERY =
+            new WbemcliUtil.WmiQuery<>("Win32_SoundDevice", AudioCardCaption.class);
+
+    public WindowsSoundCard(String kernelVersion, String name, String codec) {
+        super(kernelVersion, name, codec);
+    }
+
+    private static String getAudioCardCompleteName() {
+        WbemcliUtil.WmiResult<AudioCardCaption> r = WmiUtil.queryWMI(AUDIO_CARD_QUERY);
+        return String.valueOf(r.getValue(AudioCardCaption.MANUFACTURER, 0)) + " " + String.valueOf(r.getValue(AudioCardCaption.NAME, 0));
+    }
+
 }
