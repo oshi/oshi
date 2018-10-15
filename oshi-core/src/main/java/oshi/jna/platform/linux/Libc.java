c0afb16e0e55fd1e9627d6477e10d5fd86c99080
Daniel Widdis <widdis@gmail.com>
2018-10-14 22:08:04 -0700

    Back out JNA sysinfo bug

--- oshi-core/src/main/java/oshi/jna/platform/linux/Libc.java
+++ oshi-core/src/main/java/oshi/jna/platform/linux/Libc.java
@@ -18,7 +18,12 @@
  */
 package oshi.jna.platform.linux;
 
+import java.util.Arrays;
+import java.util.List;
+
 import com.sun.jna.Native;
+import com.sun.jna.NativeLong;
+import com.sun.jna.Structure;
 
 import oshi.jna.platform.unix.CLibrary;
 
@@ -29,5 +34,32 @@ import oshi.jna.platform.unix.CLibrary;
  * @author widdis[at]gmail[dot]com
  */
 public interface Libc extends CLibrary {
+
     Libc INSTANCE = Native.load("c", Libc.class);
+
+    class Sysinfo extends Structure {
+        public NativeLong uptime; // Seconds since boot
+        // 1, 5, and 15 minute load averages
+        public NativeLong[] loads = new NativeLong[3];
+        public NativeLong totalram; // Total usable main memory size
+        public NativeLong freeram; // Available memory size
+        public NativeLong sharedram; // Amount of shared memory
+        public NativeLong bufferram; // Memory used by buffers
+        public NativeLong totalswap; // Total swap space size
+        public NativeLong freeswap; // swap space still available
+        public short procs; // Number of current processes
+        public NativeLong totalhigh; // Total high memory size
+        public NativeLong freehigh; // Available high memory size
+        public int mem_unit; // Memory unit size in bytes
+        public byte[] _f = new byte[8]; // Won't be written for 64-bit systems
+
+        @Override
+        protected List<String> getFieldOrder() {
+            return Arrays.asList(new String[] { "uptime", "loads", "totalram", "freeram", "sharedram", "bufferram",
+                    "totalswap", "freeswap", "procs", "totalhigh", "freehigh", "mem_unit", "_f" });
+        }
+    }
+
+    int sysinfo(Sysinfo info);
+
 }
