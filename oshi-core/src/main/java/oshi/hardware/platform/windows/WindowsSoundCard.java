23405d663575589bb49eabb2f942723348065fd4
widdisd <widdisd@US-1800049065.ant.amazon.com>
2019-02-05 13:59:21 -0800

    Get Windows Sound Card info from registry

--- oshi-core/src/main/java/oshi/hardware/platform/windows/WindowsSoundCard.java
+++ oshi-core/src/main/java/oshi/hardware/platform/windows/WindowsSoundCard.java
@@ -24,62 +24,30 @@
 package oshi.hardware.platform.windows;
 
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
-import com.sun.jna.platform.win32.COM.WbemcliUtil; // NOSONAR squid:S1191
+import com.sun.jna.platform.win32.Advapi32Util; // NOSONAR squid:S1191
+import com.sun.jna.platform.win32.Win32Exception;
+import com.sun.jna.platform.win32.WinError;
+import com.sun.jna.platform.win32.WinReg;
 
 import oshi.hardware.common.AbstractSoundCard;
-import oshi.util.platform.windows.WmiQueryHandler;
-import oshi.util.platform.windows.WmiUtil;
 
 /**
- * Sound Card data obtained by Wmi Queries for Windows.
- *
- * @author : BilalAM
+ * Sound Card data obtained from registry
  */
 public class WindowsSoundCard extends AbstractSoundCard {
-    enum SoundCardName {
-        MANUFACTURER, NAME;
-    }
 
-    enum SoundCardKernel {
-        DRIVERPROVIDERNAME, DRIVERNAME, DRIVERVERSION, DEVICENAME;
-    }
+    private static final String REGISTRY_SOUNDCARDS = "SYSTEM\\CurrentControlSet\\Control\\Class\\{4d36e96c-e325-11ce-bfc1-08002be10318}\\";
 
     public WindowsSoundCard(String kernelVersion, String name, String codec) {
         super(kernelVersion, name, codec);
     }
 
     /**
-     * Short method to build our kernel string.
-     *
-     * @param index
-     *            The row to fetch data.
-     * @param cardKernelQueryResult
-     *            The result to take data from.
-     * @return The kernel string.
-     */
-    private static String getAudioCardKernelVersion(int index,
-            WbemcliUtil.WmiResult<SoundCardKernel> cardKernelQueryResult) {
-        String audioCardKernel;
-        audioCardKernel = WmiUtil.getString(cardKernelQueryResult, SoundCardKernel.DRIVERPROVIDERNAME, index) + " "
-                + WmiUtil.getString(cardKernelQueryResult, SoundCardKernel.DEVICENAME, index) + " "
-                + WmiUtil.getString(cardKernelQueryResult, SoundCardKernel.DRIVERNAME, index) + " "
-                + WmiUtil.getString(cardKernelQueryResult, SoundCardKernel.DRIVERVERSION, index);
-        return audioCardKernel;
-    }
-
-    /**
-     * Does the following :
-     * <ul>
-     * <li>Creates and runs our 'built where clause' query</li>
-     * <li>Then iterates over the key values of our map and compares the key
-     * with the DeviceName attribute returned by our where-clause query.If its a
-     * match then we create our SoundCard object</li>
-     * </ul>
-     * <br>
+     * Returns Windows audio device driver information, which represents the
+     * closest proxy we have to sound cards.
+     * <p>
      * NOTE : The reason why the codec name is same as the card name is because
      * windows does not provide the name of the codec chip but sometimes the
      * name of the card returned is infact the name of the codec chip also.
@@ -88,71 +56,29 @@ public class WindowsSoundCard extends AbstractSoundCard {
      * @return List of sound cards
      */
     public static List<WindowsSoundCard> getSoundCards() {
-        // Get the map of device manufacturer by name
-        WmiQueryHandler wmiQueryHandler = WmiQueryHandler.createInstance();
-        Map<String, String> deviceManufacturerMap = getDeviceManufacturerMap(wmiQueryHandler);
-        String driverQuery = createClause(deviceManufacturerMap);
-
-        WbemcliUtil.WmiQuery<SoundCardKernel> cardKernelQuery = new WbemcliUtil.WmiQuery<>(driverQuery,
-                SoundCardKernel.class);
-        WbemcliUtil.WmiResult<SoundCardKernel> cardKernelQueryResult = wmiQueryHandler.queryWMI(cardKernelQuery);
-
         List<WindowsSoundCard> soundCards = new ArrayList<>();
-        for (int i = 0; i < cardKernelQueryResult.getResultCount(); i++) {
-            // If the map has a key that is equal to the value returned by
-            // cardKernelQuery
-            if (deviceManufacturerMap
-                    .containsKey(WmiUtil.getString(cardKernelQueryResult, SoundCardKernel.DEVICENAME, i))) {
-                // then build a sound card by extracting values from
-                // cardKernelQuery
-                soundCards.add(new WindowsSoundCard(getAudioCardKernelVersion(i, cardKernelQueryResult),
-                        WmiUtil.getString(cardKernelQueryResult, SoundCardKernel.DRIVERPROVIDERNAME, i) + " "
-                                + WmiUtil.getString(cardKernelQueryResult, SoundCardKernel.DEVICENAME, i),
-                        WmiUtil.getString(cardKernelQueryResult, SoundCardKernel.DEVICENAME, i)));
+        String[] keys = Advapi32Util.registryGetKeys(WinReg.HKEY_LOCAL_MACHINE, REGISTRY_SOUNDCARDS);
+        for (String key : keys) {
+            String fullKey = REGISTRY_SOUNDCARDS + key;
+            try {
+                if (Advapi32Util.registryValueExists(WinReg.HKEY_LOCAL_MACHINE, fullKey, "Driver")) {
+                    soundCards.add(new WindowsSoundCard(
+                            Advapi32Util.registryGetStringValue(WinReg.HKEY_LOCAL_MACHINE, fullKey, "Driver") + " "
+                                    + Advapi32Util.registryGetStringValue(WinReg.HKEY_LOCAL_MACHINE, fullKey,
+                                            "DriverVersion"),
+                            Advapi32Util.registryGetStringValue(WinReg.HKEY_LOCAL_MACHINE, fullKey, "ProviderName")
+                                    + " "
+                                    + Advapi32Util.registryGetStringValue(WinReg.HKEY_LOCAL_MACHINE, fullKey,
+                                            "DriverDesc"),
+                            Advapi32Util.registryGetStringValue(WinReg.HKEY_LOCAL_MACHINE, fullKey, "DriverDesc")));
+                }
+            } catch (Win32Exception e) {
+                if (e.getErrorCode() != WinError.ERROR_ACCESS_DENIED) {
+                    // Ignore access denied errors, re-throw others
+                    throw e;
+                }
             }
         }
         return soundCards;
-    }
-
-    /**
-     * Gets the map of device manufacturer by name
-     * 
-     * @param wmiQueryHandler
-     * 
-     * @return THe map
-     */
-    private static Map<String, String> getDeviceManufacturerMap(WmiQueryHandler wmiQueryHandler) {
-        Map<String, String> deviceManufacturerMap = new HashMap<>();
-        WbemcliUtil.WmiQuery<SoundCardName> soundCardQuery = new WbemcliUtil.WmiQuery<>("Win32_SoundDevice",
-                SoundCardName.class);
-        WbemcliUtil.WmiResult<SoundCardName> soundCardResult = wmiQueryHandler.queryWMI(soundCardQuery);
-        for (int i = 0; i < soundCardResult.getResultCount(); i++) {
-            deviceManufacturerMap.put(WmiUtil.getString(soundCardResult, SoundCardName.NAME, i),
-                    WmiUtil.getString(soundCardResult, SoundCardName.MANUFACTURER, i));
-        }
-        return deviceManufacturerMap;
-    }
-
-    /**
-     * Creates our Win32_PnPSignedDevice query with the WHERE clause taking the
-     * attributes from our map.
-     *
-     * @param deviceManufacturerMap
-     *            The map
-     * @return The WHERE clause
-     */
-    private static String createClause(Map<String, String> deviceManufacturerMap) {
-        StringBuilder sb = new StringBuilder("Win32_PnPSignedDriver");
-        boolean first = true;
-        for (String key : deviceManufacturerMap.keySet()) {
-            if (first) {
-                sb.append(" WHERE");
-                first = false;
-            } else {
-                sb.append(" OR");
-            }
-            sb.append(" DeviceName LIKE \"%").append(key).append("%\"");
-        }
-        return sb.toString();
     }
 }
