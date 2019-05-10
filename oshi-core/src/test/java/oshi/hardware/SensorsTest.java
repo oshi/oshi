e0fad67d4f3da1873a06d1d03c4bcaeb446c03d7
Rens Louwerens <rlouwerens@gmail.com>
2019-05-10 08:36:13 +0000

    Fixed reading of sensor data

--- oshi-core/src/test/java/oshi/hardware/SensorsTest.java
+++ oshi-core/src/test/java/oshi/hardware/SensorsTest.java
@@ -1,9 +1,9 @@
 /**
  * OSHI (https://github.com/oshi/oshi)
- *
+ * <p>
  * Copyright (c) 2010 - 2019 The OSHI Project Team:
  * https://github.com/oshi/oshi/graphs/contributors
- *
+ * <p>
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
@@ -12,7 +12,7 @@
  * furnished to do so, subject to the following conditions:
  * The above copyright notice and this permission notice shall be included in
  * all copies or substantial portions of the Software.
- *
+ * <p>
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -23,28 +23,56 @@
  */
 package oshi.hardware;
 
-import static org.junit.Assert.assertTrue;
-
 import org.junit.Test;
-
 import oshi.SystemInfo;
+
+import java.io.File;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assume.assumeFalse;
+import static org.junit.Assume.assumeTrue;
 
 /**
  * Test Sensors
  */
 public class SensorsTest {
+
+
+    private SystemInfo si = new SystemInfo();
+
+    private Sensors s = si.getHardware().getSensors();
+
+    private boolean hasThermalZonePath() {
+
+        File f = new File("/sys/class/thermal/thermal_zone0");
+        return f.isDirectory();
+    }
+
     /**
-     * Test sensors
+     * Test sensors with /sys/class/thermal/thermal_zone
      */
     @Test
-    public void testSensors() {
-        SystemInfo si = new SystemInfo();
-        Sensors s = si.getHardware().getSensors();
+    public void testSensorsWithThermalZone() {
+        assumeTrue(hasThermalZonePath());
         assertTrue(s.getCpuTemperature() >= 0d && s.getCpuTemperature() <= 100d);
+        assertTrue(s.getCpuVoltage() >= 0);
+    }
+
+    /**
+     * Test sensors with /sys/class/hwmon/hwmon
+     */
+    @Test
+    public void testSensorsWithHWMon() {
+        assumeFalse(hasThermalZonePath());
+        assertTrue(s.getCpuTemperature() > 0d && s.getCpuTemperature() <= 100d);
+        assertTrue(s.getCpuVoltage() >= 0);
+    }
+
+    @Test
+    public void testFanSpeeds() {
         int[] speeds = s.getFanSpeeds();
         for (int speed : speeds) {
             assertTrue(speed >= 0);
         }
-        assertTrue(s.getCpuVoltage() >= 0);
     }
 }
