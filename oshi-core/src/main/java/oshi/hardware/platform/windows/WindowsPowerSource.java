47bf85d7e1e4d7efde741166bcf7a9206581c912
Daniel Widdis <widdis@gmail.com>
2019-10-22 12:20:31 -0700

    Ported windows example

--- oshi-core/src/main/java/oshi/hardware/platform/windows/WindowsPowerSource.java
+++ oshi-core/src/main/java/oshi/hardware/platform/windows/WindowsPowerSource.java
@@ -27,11 +27,23 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import com.sun.jna.Memory; // NOSONAR
+import com.sun.jna.Pointer;
+import com.sun.jna.platform.win32.Guid.GUID;
+import com.sun.jna.platform.win32.Kernel32;
 import com.sun.jna.platform.win32.PowrProf.POWER_INFORMATION_LEVEL;
+import com.sun.jna.platform.win32.SetupApi;
+import com.sun.jna.platform.win32.SetupApi.SP_DEVICE_INTERFACE_DATA;
+import com.sun.jna.platform.win32.WinBase;
+import com.sun.jna.platform.win32.WinError;
+import com.sun.jna.platform.win32.WinNT;
+import com.sun.jna.platform.win32.WinNT.HANDLE;
+import com.sun.jna.ptr.IntByReference;
 
 import oshi.hardware.PowerSource;
 import oshi.hardware.common.AbstractPowerSource;
 import oshi.jna.platform.windows.PowrProf;
+import oshi.jna.platform.windows.PowrProf.BATTERY_INFORMATION;
+import oshi.jna.platform.windows.PowrProf.BATTERY_QUERY_INFORMATION;
 import oshi.jna.platform.windows.PowrProf.SystemBatteryState;
 import oshi.util.FormatUtil;
 
@@ -41,6 +53,21 @@ import oshi.util.FormatUtil;
 public class WindowsPowerSource extends AbstractPowerSource {
 
     private static final Logger LOG = LoggerFactory.getLogger(WindowsPowerSource.class);
+    private static final GUID GUID_DEVCLASS_BATTERY = GUID.fromString("{72631E54-78A4-11D0-BCF7-00AA00B7B32A}");
+
+    // Returned value includes GBS_HASBATTERY if the system has a
+    // non-UPS battery, and GBS_ONBATTERY if the system is running on
+    // a battery.
+    private static final int GBS_HASBATTERY = 0x1; // dwResult & GBS_ONBATTERY means we have not yet found AC power.
+    private static final int GBS_ONBATTERY = 0x2; // dwResult & GBS_HASBATTERY means we have found a non-UPS battery.
+
+    private static final int BATTERY_SYSTEM_BATTERY = 0x80000000;
+    private static final int BATTERY_IS_SHORT_TERM = 0x20000000;
+    private static final int BATTERY_POWER_ON_LINE = 0x00000001;
+
+    private static final int IOCTL_BATTERY_QUERY_TAG = 0x294040;
+    private static final int IOCTL_BATTERY_QUERY_STATUS = 0x29404c;
+    private static final int IOCTL_BATTERY_QUERY_INFORMATION = 0x294044;
 
     /**
      * <p>
@@ -108,8 +135,6 @@ public class WindowsPowerSource extends AbstractPowerSource {
         // Kernel32.INSTANCE.DeviceIoControl(hBattery, dwIoControlCode, lpInBuffer,
         // nInBufferSize, lpOutBuffer, nOutBufferSize, lpBytesReturned, lpOverlapped)
 
-        // Enumerate battery services:
-        // https://docs.microsoft.com/en-us/windows/win32/power/enumerating-battery-devices
         int size = new SystemBatteryState().size();
         Memory mem = new Memory(size);
         if (0 == PowrProf.INSTANCE.CallNtPowerInformation(POWER_INFORMATION_LEVEL.SystemBatteryState, null, 0, mem,
@@ -130,11 +155,116 @@ public class WindowsPowerSource extends AbstractPowerSource {
         return new WindowsPowerSource("Unknown", 0d, -1d);
     }
 
+    static int GetBatteryState() {
+        // enumerate the batteries and ask each one for information.
+        // Ported from:
+        // https://docs.microsoft.com/en-us/windows/win32/power/enumerating-battery-devices
+        int dwResult = GBS_ONBATTERY;
+
+        HANDLE hdev = SetupApi.INSTANCE.SetupDiGetClassDevs(GUID_DEVCLASS_BATTERY, null, null,
+                SetupApi.DIGCF_PRESENT | SetupApi.DIGCF_DEVICEINTERFACE);
+        if (WinBase.INVALID_HANDLE_VALUE != hdev) {
+            // Limit search to 100 batteries max
+            for (int idev = 0; idev < 100; idev++) {
+                System.out.println("idev: " + idev);
+                SP_DEVICE_INTERFACE_DATA did = new SP_DEVICE_INTERFACE_DATA();
+                did.cbSize = did.size();
+
+                if (SetupApi.INSTANCE.SetupDiEnumDeviceInterfaces(hdev, null, GUID_DEVCLASS_BATTERY, idev, did)) {
+                    System.out.println("enumerating");
+                    IntByReference cbRequired = new IntByReference(0);
+                    SetupApi.INSTANCE.SetupDiGetDeviceInterfaceDetail(hdev, did, null, 0, cbRequired, null);
+                    if (WinError.ERROR_INSUFFICIENT_BUFFER == Kernel32.INSTANCE.GetLastError()) {
+                        // PSP_DEVICE_INTERFACE_DETAIL_DATA: int size + TCHAR array
+                        System.out.println("Need " + cbRequired.getValue() + " bytes");
+                        Memory pdidd = new Memory(cbRequired.getValue());
+                        pdidd.clear();
+                        pdidd.setInt(0, (int) pdidd.size()); // pdidd->cbSize = sizeof(*pdidd);
+                        System.out.println(pdidd.dump());
+                        if (SetupApi.INSTANCE.SetupDiGetDeviceInterfaceDetail(hdev, did, pdidd,
+                                cbRequired.getValue() * 2,
+                                cbRequired, null)) {
+                            System.out.println(pdidd.dump());
+                            // Enumerated a battery. Ask it for information.
+                            System.out.println("Battery: " + pdidd.getString(Integer.BYTES));
+                            HANDLE hBattery = Kernel32.INSTANCE.CreateFile(pdidd.getString(Integer.BYTES), // pdidd->DevicePath
+                                                                                                           // TODO
+                                                                                                           // TCHAR?
+                                    WinNT.GENERIC_READ | WinNT.GENERIC_WRITE,
+                                    WinNT.FILE_SHARE_READ | WinNT.FILE_SHARE_WRITE, null, WinNT.OPEN_EXISTING,
+                                    WinNT.FILE_ATTRIBUTE_NORMAL, null);
+                            if (WinBase.INVALID_HANDLE_VALUE != hBattery) {
+                                // Ask the battery for its tag.
+                                BATTERY_QUERY_INFORMATION bqi = new PowrProf.BATTERY_QUERY_INFORMATION();
+                                IntByReference dwWait = new IntByReference(0);
+                                IntByReference dwOut = new IntByReference();
+                                Pointer pTag = new IntByReference(bqi.BatteryTag).getPointer();
+
+                                if (Kernel32.INSTANCE.DeviceIoControl(hBattery, IOCTL_BATTERY_QUERY_TAG,
+                                        dwWait.getPointer(), Integer.BYTES, pTag, Integer.BYTES, dwOut, null)
+                                        && bqi.BatteryTag > 0) {
+                                    // With the tag, you can query the battery info.
+                                    BATTERY_INFORMATION bi = new BATTERY_INFORMATION();
+                                    bqi.InformationLevel = PowrProf.BATTERY_QUERY_INFORMATION_LEVEL.BatteryInformation
+                                            .ordinal();
+
+                                    if (Kernel32.INSTANCE.DeviceIoControl(hBattery, IOCTL_BATTERY_QUERY_INFORMATION,
+                                            bqi.getPointer(), bqi.size(), bi.getPointer(), bi.size(), dwOut, null)) {
+                                        // Only non-UPS system batteries count
+                                        System.out.println(bqi.toString());
+                                        System.out.println(bi.toString());
+                                        if (0 != (bi.Capabilities & BATTERY_SYSTEM_BATTERY)) {
+                                            if (0 == (bi.Capabilities & BATTERY_IS_SHORT_TERM)) {
+                                                dwResult |= GBS_HASBATTERY;
+                                            }
+
+                                            // Query the battery status.
+                                            PowrProf.BATTERY_WAIT_STATUS bws = new PowrProf.BATTERY_WAIT_STATUS();
+                                            bws.BatteryTag = bqi.BatteryTag;
+
+                                            PowrProf.BATTERY_STATUS bs = new PowrProf.BATTERY_STATUS();
+
+                                            if (Kernel32.INSTANCE.DeviceIoControl(hBattery, IOCTL_BATTERY_QUERY_STATUS,
+                                                    bws.getPointer(), bws.size(), bs.getPointer(), bs.size(), dwOut,
+                                                    null)) {
+                                                System.out.println(bs.toString());
+                                                if (0 != (bs.PowerState & BATTERY_POWER_ON_LINE)) {
+                                                    dwResult &= ~GBS_ONBATTERY;
+                                                }
+                                            }
+                                        }
+                                    }
+                                }
+                                Kernel32.INSTANCE.CloseHandle(hBattery);
+                            }
+                        }
+                    }
+                } else if (WinError.ERROR_NO_MORE_ITEMS == Kernel32.INSTANCE.GetLastError()) {
+                    break; // Enumeration failed - perhaps we're out of items
+                }
+            }
+            SetupApi.INSTANCE.SetupDiDestroyDeviceInfoList(hdev);
+        }
+
+        // Final cleanup: If we didn't find a battery, then presume that we
+        // are on AC power.
+
+        if (0 == (dwResult & GBS_HASBATTERY)) {
+            dwResult &= ~GBS_ONBATTERY;
+        }
+
+        return dwResult;
+    }
+
     /** {@inheritDoc} */
     @Override
     public void updateAttributes() {
         PowerSource ps = getPowerSource(this.name);
         this.remainingCapacity = ps.getRemainingCapacity();
         this.timeRemaining = ps.getTimeRemaining();
+    }
+
+    public static void main(String[] args) {
+        System.out.println("RESULT: " + WindowsPowerSource.GetBatteryState());
     }
 }
