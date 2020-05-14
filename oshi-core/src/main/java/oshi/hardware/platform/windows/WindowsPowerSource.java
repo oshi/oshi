9636525fdba5737c5da48fafa8f5e41f840f6a15
yadunandanbhat <yadunandanbhat@gmail.com>
2020-05-14 13:49:49 +0530

    fixed indentation

--- oshi-core/src/main/java/oshi/hardware/platform/windows/WindowsPowerSource.java
+++ oshi-core/src/main/java/oshi/hardware/platform/windows/WindowsPowerSource.java
@@ -152,7 +152,7 @@ public final class WindowsPowerSource extends AbstractPowerSource {
         HANDLE hdev = SetupApi.INSTANCE.SetupDiGetClassDevs(GUID_DEVCLASS_BATTERY, null, null,
                 SetupApi.DIGCF_PRESENT | SetupApi.DIGCF_DEVICEINTERFACE);
         if (WinBase.INVALID_HANDLE_VALUE != hdev) {
-        	boolean batteryFound = false;
+            boolean batteryFound = false;
             // Limit search to 100 batteries max
             for (int idev = 0; !batteryFound && idev < 100; idev++) {
                 SP_DEVICE_INTERFACE_DATA did = new SP_DEVICE_INTERFACE_DATA();
