93b03c51b7d999aeb14f1e73154c71b5ffb4533b
Roeezz <39015778+roeezz@users.noreply.github.com>
2020-05-16 20:43:03 +0300

    Cleanup of test messages in CentralProcessorTest.java
    -Fix some spelling mistakes and a stylistic issue with one of the messages.
    -Remove unnecessary p tags in the header.

--- oshi-core/src/test/java/oshi/hardware/CentralProcessorTest.java
+++ oshi-core/src/test/java/oshi/hardware/CentralProcessorTest.java
@@ -1,18 +1,18 @@
 /**
  * MIT License
- * <p>
+ *
  * Copyright (c) 2010 - 2020 The OSHI Project Contributors: https://github.com/oshi/oshi/graphs/contributors
- * <p>
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * <p>
+ *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * <p>
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -50,7 +50,7 @@ public class CentralProcessorTest {
 
         ProcessorIdentifier pi = p.getProcessorIdentifier();
         assertNotNull("Processor Identifier's vendor shouldn't be Null", pi.getVendor());
-        assertTrue("Processor Identifier's vendor frequency should either be -1, or a greater then 0",
+        assertTrue("Processor Identifier's vendor frequency should either be -1, or a greater than 0",
                 pi.getVendorFreq() == -1 || pi.getVendorFreq() > 0);
         assertNotNull("Processor Identifier's name shouldn't be Null", pi.getName());
         assertNotNull("Processor Identifier's identifier shouldn't be Null", pi.getIdentifier());
@@ -79,18 +79,17 @@ public class CentralProcessorTest {
             assertTrue("Central Processor's cpu number " + cpu + "'s load between ticks should be inclusively between 0 and 1",
                     p.getProcessorCpuLoadBetweenTicks(procTicks)[cpu] >= 0
                             && p.getProcessorCpuLoadBetweenTicks(procTicks)[cpu] <= 1);
-            assertEquals("Central Processor's cpu number " + cpu + " should have the same amount of cpu-load-tick" +
-                            "counters as there are TickType values",
+            assertEquals("Central Processor's cpu number " + cpu + " should have the same amount of cpu-load-tick counters as there are TickType values",
                     p.getProcessorCpuLoadTicks()[cpu].length, TickType.values().length);
         }
 
         assertTrue("Central Processor's logical processor count should be at least as high as its physical processor count",
                 p.getLogicalProcessorCount() >= p.getPhysicalProcessorCount());
-        assertTrue("Central Processor's physical processor count should by higher then 0",
+        assertTrue("Central Processor's physical processor count should by higher than 0",
                 p.getPhysicalProcessorCount() > 0);
-        assertTrue("Central Processor's physical processor count should be higher then its physical package count",
+        assertTrue("Central Processor's physical processor count should be higher than its physical package count",
                 p.getPhysicalProcessorCount() >= p.getPhysicalPackageCount());
-        assertTrue("Central Processor's physical package count should be higher then 0",
+        assertTrue("Central Processor's physical package count should be higher than 0",
                 p.getPhysicalPackageCount() > 0);
         assertTrue("Central Processor's context switch count should be 0 or higher", p.getContextSwitches() >= 0);
         assertTrue("Central Processor's interrupt count should be 0 or higher", p.getInterrupts() >= 0);
