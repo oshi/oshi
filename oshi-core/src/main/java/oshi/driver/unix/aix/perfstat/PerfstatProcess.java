dcde6b045625e1eeb6a9fbca8fa826a8a0b0fd9c
Daniel Widdis <widdis@gmail.com>
2020-07-05 13:13:36 -0700

    Update PerfstatProcess main method to output only the stats we care about porting

--- oshi-core/src/main/java/oshi/driver/unix/aix/perfstat/PerfstatProcess.java
+++ oshi-core/src/main/java/oshi/driver/unix/aix/perfstat/PerfstatProcess.java
@@ -65,10 +65,11 @@ public final class PerfstatProcess {
 
     public static void main(String[] args) {
         perfstat_process_t[] procs = queryProcesses();
-        System.out.println("Found" + procs.length + " process(es)");
-        for (int i = 0; i < procs.length && i < 10; i++) {
-            System.out.format("%s: pid=%d, VSZ=%d KB, RSS=%d KB (maybe?), ucpu_time=%d, scpu_time=%d%n",
-                    Native.toString(procs[i].proc_name), procs[i].pid, procs[i].proc_size, procs[i].real_inuse,
+        System.out.println("Found " + procs.length + " process(es)");
+        for (int i = 0; i < procs.length; i++) {
+            System.out.format("%s: pid=%d, timebase=%d, ucpu_time=%d, scpu_time=%d%n",
+                    Native.toString(procs[i].proc_name), procs[i].pid,
+                    procs[i].last_timebase,
                     (long) procs[i].ucpu_time, (long) procs[i].scpu_time);
         }
     }
