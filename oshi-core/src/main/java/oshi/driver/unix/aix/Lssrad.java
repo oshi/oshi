e196ae816283f57f17485740e3bc701d7d36de9e
Daniel Widdis <widdis@gmail.com>
2020-07-07 12:28:13 -0700

    Map AIX srad nodes to processors (#1291)

---
+++ oshi-core/src/main/java/oshi/driver/unix/aix/Lssrad.java
@@ -0,0 +1,91 @@
+/**
+ * MIT License
+ *
+ * Copyright (c) 2010 - 2020 The OSHI Project Contributors: https://github.com/oshi/oshi/graphs/contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package oshi.driver.unix.aix;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import oshi.annotation.concurrent.ThreadSafe;
+import oshi.util.ExecutingCommand;
+import oshi.util.ParseUtil;
+import oshi.util.tuples.Pair;
+
+/**
+ * Utility to query lssrad
+ */
+@ThreadSafe
+public final class Lssrad {
+
+    private Lssrad() {
+    }
+
+    /**
+     * Query {@code lssrad} to get numa node and physical package info
+     *
+     * @return A map of processor number to a pair containing the ref (NUMA
+     *         equivalent) and srad (package)
+     */
+    public static Map<Integer, Pair<Integer, Integer>> queryNodesPackages() {
+        /*-
+        # lssrad -av
+        REF1        SRAD        MEM        CPU
+        0
+                       0       32749.12    0-63
+                       1        9462.00    64-67 72-75
+                                           80-83 88-91
+        1
+                       2        2471.19    92-95
+        2
+                       3        1992.00
+                       4         249.00
+         */
+        int node = 0;
+        int slot = 0;
+        Map<Integer, Pair<Integer, Integer>> nodeMap = new HashMap<>();
+        List<String> lssrad = ExecutingCommand.runNative("lssrad -av");
+        // remove header
+        if (!lssrad.isEmpty()) {
+            lssrad.remove(0);
+        }
+        for (String s : lssrad) {
+            String t = s.trim();
+            if (!t.isEmpty()) {
+                if (Character.isDigit(s.charAt(0))) {
+                    node = ParseUtil.parseIntOrDefault(t, 0);
+                } else {
+                    if (t.contains(".")) {
+                        String[] split = ParseUtil.whitespaces.split(t, 3);
+                        slot = ParseUtil.parseIntOrDefault(split[0], 0);
+                        t = split.length > 2 ? split[2] : "";
+                    }
+                    for (Integer proc : ParseUtil.parseHyphenatedIntList(t)) {
+                        nodeMap.put(proc, new Pair<>(node, slot));
+                    }
+                }
+            }
+        }
+        return nodeMap;
+    }
+}
