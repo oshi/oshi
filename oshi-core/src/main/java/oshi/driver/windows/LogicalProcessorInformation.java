20c2a159cb802e2353a6912b0b2111245e961290
Daniel Widdis <widdis@gmail.com>
2020-07-08 07:59:30 -0700

    Refactor Windows processor counting # Conflicts: #	oshi-core/src/main/java/oshi/hardware/platform/windows/WindowsCentralProcessor.java

--- oshi-core/src/main/java/oshi/driver/windows/LogicalProcessorInformation.java
+++ oshi-core/src/main/java/oshi/driver/windows/LogicalProcessorInformation.java
@@ -24,11 +24,17 @@
 package oshi.driver.windows;
 
 import java.util.ArrayList;
+import java.util.Comparator;
 import java.util.List;
 
 import com.sun.jna.platform.win32.Kernel32Util; // NOSONAR squid:S1191
 import com.sun.jna.platform.win32.WinNT;
+import com.sun.jna.platform.win32.WinNT.GROUP_AFFINITY;
+import com.sun.jna.platform.win32.WinNT.LOGICAL_PROCESSOR_RELATIONSHIP;
+import com.sun.jna.platform.win32.WinNT.NUMA_NODE_RELATIONSHIP;
+import com.sun.jna.platform.win32.WinNT.PROCESSOR_RELATIONSHIP;
 import com.sun.jna.platform.win32.WinNT.SYSTEM_LOGICAL_PROCESSOR_INFORMATION;
+import com.sun.jna.platform.win32.WinNT.SYSTEM_LOGICAL_PROCESSOR_INFORMATION_EX;
 
 import oshi.annotation.concurrent.ThreadSafe;
 import oshi.hardware.CentralProcessor.LogicalProcessor;
@@ -41,6 +47,101 @@ public final class LogicalProcessorInformation {
 
     private LogicalProcessorInformation() {
     }
+
+    /**
+     * Get a list of logical processors on this machine. Requires Windows 7 and
+     * higher.
+     *
+     * @return A list of logical processors
+     */
+    public static List<LogicalProcessor> getLogicalProcessorInformationEx() {
+        // Collect a list of logical processors on each physical core and
+        // package. These will be 64-bit bitmasks.
+        SYSTEM_LOGICAL_PROCESSOR_INFORMATION_EX[] procInfo = Kernel32Util
+                .getLogicalProcessorInformationEx(WinNT.LOGICAL_PROCESSOR_RELATIONSHIP.RelationAll);
+        List<GROUP_AFFINITY[]> packages = new ArrayList<>();
+        List<NUMA_NODE_RELATIONSHIP> numaNodes = new ArrayList<>();
+        List<GROUP_AFFINITY> cores = new ArrayList<>();
+
+        for (int i = 0; i < procInfo.length; i++) {
+            switch (procInfo[i].relationship) {
+            case LOGICAL_PROCESSOR_RELATIONSHIP.RelationProcessorPackage:
+                packages.add(((PROCESSOR_RELATIONSHIP) procInfo[i]).groupMask);
+                break;
+            case LOGICAL_PROCESSOR_RELATIONSHIP.RelationNumaNode:
+                numaNodes.add((NUMA_NODE_RELATIONSHIP) procInfo[i]);
+                break;
+            case LOGICAL_PROCESSOR_RELATIONSHIP.RelationProcessorCore:
+                cores.add(((PROCESSOR_RELATIONSHIP) procInfo[i]).groupMask[0]);
+                break;
+            default:
+                // Ignore Group and Cache info
+                break;
+            }
+        }
+        // Windows doesn't define core and package numbers, so we sort the lists
+        // so core and package numbers increment consistently with processor
+        // numbers/bitmasks, ordered in groups
+        cores.sort(Comparator.comparing(c -> c.group * 64L + c.mask.longValue()));
+        packages.sort(Comparator.comparing(p -> p[0].group * 64L + p[0].mask.longValue()));
+
+        // Iterate Logical Processors and use bitmasks to match packages, cores,
+        // and NUMA nodes
+        List<LogicalProcessor> logProcs = new ArrayList<>();
+        for (GROUP_AFFINITY coreMask : cores) {
+            int group = coreMask.group;
+            long mask = coreMask.mask.longValue();
+            // Iterate mask for logical processor numbers
+            int lowBit = Long.numberOfTrailingZeros(mask);
+            int hiBit = 63 - Long.numberOfLeadingZeros(mask);
+            for (int lp = lowBit; lp <= hiBit; lp++) {
+                if ((mask & (1L << lp)) > 0) {
+                    LogicalProcessor logProc = new LogicalProcessor(lp, getMatchingCore(cores, group, lp),
+                            getMatchingPackage(packages, group, lp), getMatchingNumaNode(numaNodes, group, lp), group);
+                    logProcs.add(logProc);
+                }
+            }
+        }
+        // Sort by numaNode and then logical processor number to match
+        // PerfCounter/WMI ordering
+        logProcs.sort(Comparator.comparing(LogicalProcessor::getNumaNode)
+                .thenComparing(LogicalProcessor::getProcessorNumber));
+        return logProcs;
+    }
+
+    private static int getMatchingPackage(List<GROUP_AFFINITY[]> packages, int g, int lp) {
+        for (int i = 0; i < packages.size(); i++) {
+            for (int j = 0; j < packages.get(i).length; j++) {
+                if ((packages.get(i)[j].mask.longValue() & (1L << lp)) > 0 && packages.get(i)[j].group == g) {
+                    return i;
+                }
+            }
+        }
+        return 0;
+    }
+
+    private static int getMatchingNumaNode(List<NUMA_NODE_RELATIONSHIP> numaNodes, int g, int lp) {
+        for (int j = 0; j < numaNodes.size(); j++) {
+            if ((numaNodes.get(j).groupMask.mask.longValue() & (1L << lp)) > 0
+                    && numaNodes.get(j).groupMask.group == g) {
+                return numaNodes.get(j).nodeNumber;
+            }
+        }
+        return 0;
+    }
+
+    private static int getMatchingCore(List<GROUP_AFFINITY> cores, int g, int lp) {
+        for (int j = 0; j < cores.size(); j++) {
+            if ((cores.get(j).mask.longValue() & (1L << lp)) > 0 && cores.get(j).group == g) {
+                return j;
+            }
+        }
+        return 0;
+    }
+
+    /*
+     * Non-EX version for pre-Win7
+     */
 
     /**
      * Get a list of logical processors on this machine
--- oshi-core/src/main/java/oshi/hardware/platform/windows/WindowsCentralProcessor.java
+++ oshi-core/src/main/java/oshi/hardware/platform/windows/WindowsCentralProcessor.java
@@ -23,9 +23,7 @@
  */
 package oshi.hardware.platform.windows;
 
-import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -37,17 +35,10 @@ import com.sun.jna.Memory; // NOSONAR squid:S1191
 import com.sun.jna.Native;
 import com.sun.jna.platform.win32.Advapi32Util;
 import com.sun.jna.platform.win32.Kernel32;
-import com.sun.jna.platform.win32.Kernel32Util;
 import com.sun.jna.platform.win32.PowrProf.POWER_INFORMATION_LEVEL;
 import com.sun.jna.platform.win32.VersionHelpers;
 import com.sun.jna.platform.win32.WinBase;
 import com.sun.jna.platform.win32.WinBase.SYSTEM_INFO;
-import com.sun.jna.platform.win32.WinNT;
-import com.sun.jna.platform.win32.WinNT.GROUP_AFFINITY;
-import com.sun.jna.platform.win32.WinNT.LOGICAL_PROCESSOR_RELATIONSHIP;
-import com.sun.jna.platform.win32.WinNT.NUMA_NODE_RELATIONSHIP;
-import com.sun.jna.platform.win32.WinNT.PROCESSOR_RELATIONSHIP;
-import com.sun.jna.platform.win32.WinNT.SYSTEM_LOGICAL_PROCESSOR_INFORMATION_EX;
 import com.sun.jna.platform.win32.WinReg;
 import com.sun.jna.platform.win32.COM.WbemcliUtil.WmiResult;
 
@@ -155,102 +146,18 @@ final class WindowsCentralProcessor extends AbstractCentralProcessor {
     @Override
     protected LogicalProcessor[] initProcessorCounts() {
         if (VersionHelpers.IsWindows7OrGreater()) {
-            return getLogicalProcessorInformationEx();
+            List<LogicalProcessor> logProcs = LogicalProcessorInformation.getLogicalProcessorInformationEx();
+            // Save numaNode,Processor lookup for future PerfCounter instance lookup
+            this.numaNodeProcToLogicalProcMap = new HashMap<>();
+            int lp = 0;
+            for (LogicalProcessor logProc : logProcs) {
+                numaNodeProcToLogicalProcMap
+                        .put(String.format("%d,%d", logProc.getNumaNode(), logProc.getProcessorNumber()), lp++);
+            }
+            return logProcs.toArray(new LogicalProcessor[0]);
         } else {
             return LogicalProcessorInformation.getLogicalProcessorInformation().toArray(new LogicalProcessor[0]);
         }
-    }
-
-    private LogicalProcessor[] getLogicalProcessorInformationEx() {
-        // Collect a list of logical processors on each physical core and
-        // package. These will be 64-bit bitmasks.
-        SYSTEM_LOGICAL_PROCESSOR_INFORMATION_EX[] procInfo = Kernel32Util
-                .getLogicalProcessorInformationEx(WinNT.LOGICAL_PROCESSOR_RELATIONSHIP.RelationAll);
-        List<GROUP_AFFINITY[]> packages = new ArrayList<>();
-        List<NUMA_NODE_RELATIONSHIP> numaNodes = new ArrayList<>();
-        List<GROUP_AFFINITY> cores = new ArrayList<>();
-
-        for (int i = 0; i < procInfo.length; i++) {
-            switch (procInfo[i].relationship) {
-            case LOGICAL_PROCESSOR_RELATIONSHIP.RelationProcessorPackage:
-                packages.add(((PROCESSOR_RELATIONSHIP) procInfo[i]).groupMask);
-                break;
-            case LOGICAL_PROCESSOR_RELATIONSHIP.RelationNumaNode:
-                numaNodes.add((NUMA_NODE_RELATIONSHIP) procInfo[i]);
-                break;
-            case LOGICAL_PROCESSOR_RELATIONSHIP.RelationProcessorCore:
-                cores.add(((PROCESSOR_RELATIONSHIP) procInfo[i]).groupMask[0]);
-                break;
-            default:
-                // Ignore Group and Cache info
-                break;
-            }
-        }
-        // Windows doesn't define core and package numbers, so we sort the lists
-        // so core and package numbers increment consistently with processor
-        // numbers/bitmasks, ordered in groups
-        cores.sort(Comparator.comparing(c -> c.group * 64L + c.mask.longValue()));
-        packages.sort(Comparator.comparing(p -> p[0].group * 64L + p[0].mask.longValue()));
-
-        // Iterate Logical Processors and use bitmasks to match packages, cores,
-        // and NUMA nodes
-        List<LogicalProcessor> logProcs = new ArrayList<>();
-        for (GROUP_AFFINITY coreMask : cores) {
-            int group = coreMask.group;
-            long mask = coreMask.mask.longValue();
-            // Iterate mask for logical processor numbers
-            int lowBit = Long.numberOfTrailingZeros(mask);
-            int hiBit = 63 - Long.numberOfLeadingZeros(mask);
-            for (int lp = lowBit; lp <= hiBit; lp++) {
-                if ((mask & (1L << lp)) > 0) {
-                    LogicalProcessor logProc = new LogicalProcessor(lp, getMatchingCore(cores, group, lp),
-                            getMatchingPackage(packages, group, lp), getMatchingNumaNode(numaNodes, group, lp), group);
-                    logProcs.add(logProc);
-                }
-            }
-        }
-        // Sort by numaNode and then logical processor number to match
-        // PerfCounter/WMI ordering
-        logProcs.sort(Comparator.comparing(LogicalProcessor::getNumaNode)
-                .thenComparing(LogicalProcessor::getProcessorNumber));
-        // Save numaNode,Processor lookup for future PerfCounter instance lookup
-        int lp = 0;
-        this.numaNodeProcToLogicalProcMap = new HashMap<>();
-        for (LogicalProcessor logProc : logProcs) {
-            numaNodeProcToLogicalProcMap
-                    .put(String.format("%d,%d", logProc.getNumaNode(), logProc.getProcessorNumber()), lp++);
-        }
-        return logProcs.toArray(new LogicalProcessor[0]);
-    }
-
-    private static int getMatchingPackage(List<GROUP_AFFINITY[]> packages, int g, int lp) {
-        for (int i = 0; i < packages.size(); i++) {
-            for (int j = 0; j < packages.get(i).length; j++) {
-                if ((packages.get(i)[j].mask.longValue() & (1L << lp)) > 0 && packages.get(i)[j].group == g) {
-                    return i;
-                }
-            }
-        }
-        return 0;
-    }
-
-    private static int getMatchingNumaNode(List<NUMA_NODE_RELATIONSHIP> numaNodes, int g, int lp) {
-        for (int j = 0; j < numaNodes.size(); j++) {
-            if ((numaNodes.get(j).groupMask.mask.longValue() & (1L << lp)) > 0
-                    && numaNodes.get(j).groupMask.group == g) {
-                return numaNodes.get(j).nodeNumber;
-            }
-        }
-        return 0;
-    }
-
-    private static int getMatchingCore(List<GROUP_AFFINITY> cores, int g, int lp) {
-        for (int j = 0; j < cores.size(); j++) {
-            if ((cores.get(j).mask.longValue() & (1L << lp)) > 0 && cores.get(j).group == g) {
-                return j;
-            }
-        }
-        return 0;
     }
 
     @Override
