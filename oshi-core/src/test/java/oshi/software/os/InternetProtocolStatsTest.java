1614c1ba3c404730f2b6336d645b2025728d243c
Jordan Duabe <1442368+j4ckofalltrades@users.noreply.github.com>
2020-10-11 23:09:27 +0800

    Add messages to unit test assertions
    Update test assertions for classes in the oshi-core/src/test/software.os
    package.
    
    Refs #1215

--- oshi-core/src/test/java/oshi/software/os/InternetProtocolStatsTest.java
+++ oshi-core/src/test/java/oshi/software/os/InternetProtocolStatsTest.java
@@ -45,44 +45,46 @@ public class InternetProtocolStatsTest {
         SystemInfo si = new SystemInfo();
         InternetProtocolStats ipStats = si.getOperatingSystem().getInternetProtocolStats();
         TcpStats tcp4 = ipStats.getTCPv4Stats();
-        assertTrue(tcp4.getConnectionsEstablished() >= 0);
-        assertTrue(tcp4.getConnectionsActive() >= 0);
-        assertTrue(tcp4.getConnectionsPassive() >= 0);
-        assertTrue(tcp4.getConnectionFailures() >= 0);
-        assertTrue(tcp4.getConnectionsReset() >= 0);
-        assertTrue(tcp4.getSegmentsReceived() >= 0);
-        assertTrue(tcp4.getSegmentsSent() >= 0);
-        assertTrue(tcp4.getSegmentsRetransmitted() >= 0);
-        assertTrue(tcp4.getInErrors() >= 0);
-        assertTrue(tcp4.getOutResets() >= 0);
+        assertTrue("IPV4 TCP connections should be 0 or higher", tcp4.getConnectionsEstablished() >= 0);
+        assertTrue("IPV4 TCP active connections should be 0 or higher", tcp4.getConnectionsActive() >= 0);
+        assertTrue("IPV4 TCP passive connections should be 0 or higher", tcp4.getConnectionsPassive() >= 0);
+        assertTrue("IPV4 TCP connection failures should be 0 or higher", tcp4.getConnectionFailures() >= 0);
+        assertTrue("IPV4 TCP connections reset should be 0 or higher", tcp4.getConnectionsReset() >= 0);
+        assertTrue("IPV4 TCP segments received should be 0 or higher", tcp4.getSegmentsReceived() >= 0);
+        assertTrue("IPV4 TCP segments sent should be 0 or higher", tcp4.getSegmentsSent() >= 0);
+        assertTrue("IPV4 TCP segments retransmitted should be 0 or higher", tcp4.getSegmentsRetransmitted() >= 0);
+        assertTrue("IPV4 TCP errors should be or higher ", tcp4.getInErrors() >= 0);
+        assertTrue("IPV4 TCP segments transmitted with the reset flag should be 0 or higher", tcp4.getOutResets() >= 0);
 
         TcpStats tcp6 = ipStats.getTCPv6Stats();
-        assertTrue(tcp6.getConnectionsEstablished() >= 0);
-        assertTrue(tcp6.getConnectionsActive() >= 0);
-        assertTrue(tcp6.getConnectionsPassive() >= 0);
-        assertTrue(tcp6.getConnectionFailures() >= 0);
-        assertTrue(tcp6.getConnectionsReset() >= 0);
-        assertTrue(tcp6.getSegmentsReceived() >= 0);
-        assertTrue(tcp6.getSegmentsSent() >= 0);
-        assertTrue(tcp6.getSegmentsRetransmitted() >= 0);
-        assertTrue(tcp6.getInErrors() >= 0);
-        assertTrue(tcp6.getOutResets() >= 0);
+        assertTrue("IPV6 TCP connections should be 0 or higher", tcp6.getConnectionsEstablished() >= 0);
+        assertTrue("IPV6 TCP active connections should be 0 or higher", tcp6.getConnectionsActive() >= 0);
+        assertTrue("IPV6 TCP passive connections should be 0 or higher", tcp6.getConnectionsPassive() >= 0);
+        assertTrue("IPV6 TCP connection failures should be 0 or higher", tcp6.getConnectionFailures() >= 0);
+        assertTrue("IPV6 TCP connections reset should be 0 or higher", tcp6.getConnectionsReset() >= 0);
+        assertTrue("IPV6 TCP segments received should be 0 or higher", tcp6.getSegmentsReceived() >= 0);
+        assertTrue("IPV6 TCP segments sent should be 0 or higher", tcp6.getSegmentsSent() >= 0);
+        assertTrue("IPV6 TCP segments retransmitted should be 0 or higher", tcp6.getSegmentsRetransmitted() >= 0);
+        assertTrue("IPV6 TCP errors should be or higher ", tcp6.getInErrors() >= 0);
+        assertTrue("IPV6 TCP segments transmitted with the reset flag should be 0 or higher", tcp6.getOutResets() >= 0);
 
         UdpStats udp4 = ipStats.getUDPv4Stats();
-        assertTrue(udp4.getDatagramsReceived() >= 0);
-        assertTrue(udp4.getDatagramsSent() >= 0);
-        assertTrue(udp4.getDatagramsNoPort() >= 0);
-        assertTrue(udp4.getDatagramsReceivedErrors() >= 0);
+        assertTrue("IPV4 UDP datagrams delivered to UDP users should be 0 or higher", udp4.getDatagramsReceived() >= 0);
+        assertTrue("IPV4 UDP datagrams sent should be 0 or higher", udp4.getDatagramsSent() >= 0);
+        assertTrue("IPV4 UDP datagrams received with no application at the destination port should be 0 or higher", udp4.getDatagramsNoPort() >= 0);
+        assertTrue("IPV4 UDP datagrams received that could not be delivered for reasons other than lack of application at the destination port should be 0 or higher",
+                udp4.getDatagramsReceivedErrors() >= 0);
 
         UdpStats udp6 = ipStats.getUDPv6Stats();
-        assertTrue(udp6.getDatagramsReceived() >= 0);
-        assertTrue(udp6.getDatagramsSent() >= 0);
-        assertTrue(udp6.getDatagramsNoPort() >= 0);
-        assertTrue(udp6.getDatagramsReceivedErrors() >= 0);
+        assertTrue("IPV6 UDP datagrams delivered to UDP users should be 0 or higher", udp6.getDatagramsReceived() >= 0);
+        assertTrue("IPV6 UDP datagrams sent should be 0 or higher", udp6.getDatagramsSent() >= 0);
+        assertTrue("IPV6 UDP datagrams received with no application at the destination port should be 0 or higher", udp6.getDatagramsNoPort() >= 0);
+        assertTrue("IPV6 UDP datagrams received that could not be delivered for reasons other than lack of application at the destination port should be 0 or higher",
+                udp6.getDatagramsReceivedErrors() >= 0);
 
-        assertNotNull(tcp4.toString());
-        assertNotNull(tcp6.toString());
-        assertNotNull(udp4.toString());
-        assertNotNull(udp6.toString());
+        assertNotNull("IPV4 TCP stats shouldn't be null", tcp4.toString());
+        assertNotNull("IPV6 TCP stats shouldn't be null", tcp6.toString());
+        assertNotNull("IPV4 UDP stats shouldn't be null", udp4.toString());
+        assertNotNull("IPV6 UDP stats shouldn't be null", udp6.toString());
     }
 }
