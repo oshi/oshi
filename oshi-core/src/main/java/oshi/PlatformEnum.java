ae641b43feb4247f3cfa420911444a342a6a1c56
Daniel Widdis <widdis@gmail.com>
2021-09-05 14:37:34 -0700

    Align PlatformEnum to JNA Platform type (#1712)

--- oshi-core/src/main/java/oshi/PlatformEnum.java
+++ oshi-core/src/main/java/oshi/PlatformEnum.java
@@ -24,46 +24,109 @@
 package oshi;
 
 /**
- * Enum of supported operating systems.
+ * An enumeration of supported operating systems. The order of declaration
+ * matches the osType constants in the JNA Platform class.
  */
 public enum PlatformEnum {
     /**
-     * Microsoft Windows
+     * macOS
      */
-    WINDOWS,
+    MACOS("macOS"),
     /**
      * A flavor of Linux
      */
-    LINUX,
+    LINUX("Linux"),
     /**
-     * macOS (formerly OS X)
+     * Microsoft Windows
      */
-    MACOS,
-    /**
-     * Mac OS X
-     *
-     * @deprecated Use {@link #MACOS}
-     */
-    @Deprecated
-    MACOSX,
+    WINDOWS("Windows"),
     /**
      * Solaris (SunOS)
      */
-    SOLARIS,
+    SOLARIS("Solaris"),
     /**
      * FreeBSD
      */
-    FREEBSD,
-    /**
-     * IBM AIX
-     */
-    AIX,
+    FREEBSD("FreeBSD"),
     /**
      * OpenBSD
      */
-    OPENBSD,
+    OPENBSD("OpenBSD"),
     /**
-     * WindowsCE, or an unspecified system
+     * Windows Embedded Compact
      */
-    UNKNOWN;
+    WINDOWSCE("Windows CE"),
+    /**
+     * IBM AIX
+     */
+    AIX("AIX"),
+    /**
+     * Android
+     */
+    ANDROID("Android"),
+    /**
+     * GNU operating system
+     */
+    GNU("GNU"),
+    /**
+     * Debian GNU/kFreeBSD
+     */
+    KFREEBSD("kFreeBSD"),
+    /**
+     * NetBSD
+     */
+    NETBSD("NetBSD"),
+    /**
+     * An unspecified system
+     */
+    UNKNOWN("Unknown"),
+    /**
+     * Legacy name for Mac OS version 10.x
+     *
+     * @deprecated use {@link MACOS}
+     */
+    @Deprecated
+    MACOSX("macOS");
+
+    private String name;
+
+    PlatformEnum(String name) {
+        this.name = name;
+    }
+
+    /**
+     * Gets the friendly name of the platform
+     *
+     * @return the friendly name of the platform
+     */
+    public String getName() {
+        return this.name;
+    }
+
+    /**
+     * Gets the friendly name of the specified JNA Platform type
+     *
+     * @param osType
+     *            The constant returned from JNA's
+     *            {@link com.sun.jna.Platform#getOSType()} method.
+     * @return the friendly name of the specified JNA Platform type
+     */
+    public static String getName(int osType) {
+        return getValue(osType).getName();
+    }
+
+    /**
+     * Gets the value corresponding to the specified JNA Platform type
+     *
+     * @param osType
+     *            The constant returned from JNA's
+     *            {@link com.sun.jna.Platform#getOSType()} method.
+     * @return the value corresponding to the specified JNA Platform type
+     */
+    public static PlatformEnum getValue(int osType) {
+        if (osType < 0 || osType >= UNKNOWN.ordinal()) {
+            return UNKNOWN;
+        }
+        return values()[osType];
+    }
 }
