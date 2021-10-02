4ceb58ff63fedc725d5f37e5f935af10d996c743
Pratham Gandhi <ppg.1382@gmail.com>
2021-10-02 23:32:24 +0530

    Removed unused interrupt

--- oshi-core/src/main/java/oshi/software/os/mac/MacOSProcess.java
+++ oshi-core/src/main/java/oshi/software/os/mac/MacOSProcess.java
@@ -40,8 +40,6 @@ import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.function.Supplier;
-import java.util.Arrays;
-import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
