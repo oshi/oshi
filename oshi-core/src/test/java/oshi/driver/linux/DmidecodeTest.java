7f14e163537a2753681869822ac555d3129b656b
adrian <35755741+adrian-kong@users.noreply.github.com>
2021-10-05 13:06:04 +1100

    LshwTest and DmidecodeTest for Linux Driver (#1734)
    * Changed return type of getServices() in OperatingSystem
    
    * Changed return type of getServices() in OperatingSystem
    
    * Minor fixes
    
    * Minor fixes
    
    * Escaping angled brackets
    
    * Added DmidecodeTest + LshwTest

---
+++ oshi-core/src/test/java/oshi/driver/linux/DmidecodeTest.java
@@ -0,0 +1,61 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2010 - 2021 The OSHI Project Contributors: https://github.com/oshi/oshi/graphs/contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package oshi.driver.linux;
+
+import com.sun.jna.Platform;
+import org.junit.Test;
+import oshi.util.tuples.Pair;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.anyOf;
+import static org.hamcrest.Matchers.nullValue;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.emptyString;
+import static org.hamcrest.Matchers.containsString;
+
+public class DmidecodeTest {
+
+    @Test
+    public void testQuerySerialNumber() {
+        if (Platform.isLinux()) {
+            assertThat("Test Dmidecode querySerialNumber", Dmidecode.querySerialNumber(), anyOf(nullValue(), not(emptyString())));
+        }
+    }
+
+    @Test
+    public void testQueryUUID() {
+        if (Platform.isLinux()) {
+            assertThat("Test Dmidecode queryUUID", Dmidecode.querySerialNumber(), anyOf(nullValue(), not(emptyString())));
+        }
+    }
+
+    @Test
+    public void testQueryBiosNameRev() {
+        if (Platform.isLinux()) {
+            Pair<String, String> biosNameRev = Dmidecode.queryBiosNameRev();
+            assertThat("Test Dmidecode queryBiosNameRev biosName", biosNameRev.getA(), anyOf(nullValue(), containsString(" ")));
+            assertThat("Test Dmidecode queryBiosNameRev revision", biosNameRev.getA(), anyOf(nullValue(), not(emptyString())));
+        }
+    }
+}
