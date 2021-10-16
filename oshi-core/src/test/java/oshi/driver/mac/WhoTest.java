d38c42d91bef140b49613e39e0e3a8199263fb56
TonyFeng <brightfz1@gmail.com>
2021-10-15 20:34:25 -0400

    Add WhoTest for mac driver (#1742)

---
+++ oshi-core/src/test/java/oshi/driver/mac/WhoTest.java
@@ -0,0 +1,45 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2010 - 2021 The OSHI Project Contributors: https://github.com/oshi/oshi/graphs/contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package oshi.driver.mac;
+
+import com.sun.jna.Platform;
+import org.junit.Test;
+import oshi.software.os.OSSession;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+public class WhoTest {
+    @Test
+    public void testQueryUtxent() {
+        if (Platform.isMac()) {
+            for (OSSession session : Who.queryUtxent()) {
+                assertThat("Session login time should be greater than 0", session.getLoginTime(), is(greaterThan(0L)));
+                assertThat("Session login time should be less than current time", session.getLoginTime(), is(lessThan(System.currentTimeMillis())));
+                assertThat("User should be non-empty", session.getUserName(), is(not(emptyOrNullString())));
+                assertThat("Devices should be non-empty", session.getTerminalDevice(), is(not(emptyOrNullString())));
+            }
+        }
+    }
+}
