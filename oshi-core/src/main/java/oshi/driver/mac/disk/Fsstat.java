4209246af3537321b8a3b101b676ca5567251e5d
sydneynachinger <32098695+sydneynachinger@users.noreply.github.com>
2021-10-16 12:44:59 -0400

    Rewriting test to use mockito inline so that queryPartitionToMountMap() is testable with helper functions mocked

--- oshi-core/src/main/java/oshi/driver/mac/disk/Fsstat.java
+++ oshi-core/src/main/java/oshi/driver/mac/disk/Fsstat.java
@@ -37,16 +37,16 @@ import oshi.annotation.concurrent.ThreadSafe;
  * Utility to query fsstat
  */
 @ThreadSafe
-public class Fsstat {
+public final class Fsstat {
 
-    Fsstat() {
+    private Fsstat() {
     }
 
-    public int queryFsstat(Statfs[] buf, int bufsize, int flags) {
+    public static int queryFsstat(Statfs[] buf, int bufsize, int flags) {
         return SystemB.INSTANCE.getfsstat64(buf, bufsize, flags);
     }
 
-    public Statfs[] getFileSystems(int numfs) {
+    public static Statfs[] getFileSystems(int numfs) {
         // Create array to hold results
         Statfs[] fs = new Statfs[numfs];
         // Write file system data to array
@@ -59,16 +59,15 @@ public class Fsstat {
      * @return A map with partitions as the key and mount points as the value
      */
     public static Map<String, String> queryPartitionToMountMap() {
-        Fsstat fsstat = new Fsstat();
         Map<String, String> mountPointMap = new HashMap<>();
 
         // Use statfs to get size of mounted file systems
-        int numfs = fsstat.queryFsstat(null, 0, 0);
-        Statfs[] fs = fsstat.getFileSystems(numfs);
+        int numfs = queryFsstat(null, 0, 0);
+        System.out.print(numfs);
+        Statfs[] fs = getFileSystems(numfs);
 
         // Iterate all mounted file systems
         for (Statfs f : fs) {
-            System.out.print(f);
             String mntFrom = Native.toString(f.f_mntfromname, StandardCharsets.UTF_8);
             mountPointMap.put(mntFrom.replace("/dev/", ""), Native.toString(f.f_mntonname, StandardCharsets.UTF_8));
         }
--- oshi-core/src/test/java/oshi/driver/mac/disk/FsstatTest.java
+++ oshi-core/src/test/java/oshi/driver/mac/disk/FsstatTest.java
@@ -4,29 +4,42 @@ import com.sun.jna.Platform;
 import com.sun.jna.platform.mac.SystemB;
 import org.junit.Assert;
 import org.junit.jupiter.api.Test;
+import org.mockito.MockedStatic;
 import org.mockito.Mockito;
 
 
-import static org.mockito.Mockito.when;
+import java.util.HashMap;
+import java.util.Map;
+
 
 public class FsstatTest {
+
 
 
     @Test
     void testQueryPartitionToMountMap(){
         if(Platform.isMac()){
             int mockNumfs = 1;
-            SystemB.Statfs[] mockFs = new SystemB.Statfs[mockNumfs];
+            SystemB.Statfs[] mockFileSystems = new SystemB.Statfs[mockNumfs];
             SystemB.Statfs fs = new SystemB.Statfs();
-            fs.f_mntfromname ="/test/dev".getBytes();
-            mockFs[0] = fs;
+            fs.f_mntfromname ="/test/dev/".getBytes();
+            fs.f_mntonname = "/hello".getBytes();
+            mockFileSystems[0] = fs;
 
-            Fsstat fsstatSpy = Mockito.spy(new Fsstat());
-            when(fsstatSpy.queryFsstat(null,0,0)).thenReturn(mockNumfs);
-            when(fsstatSpy.getFileSystems(mockNumfs)).thenReturn(mockFs);
+            Map<String, String> expeectedMountPointMap = new HashMap<>();
+            expeectedMountPointMap.put("/test","/hello");
 
-            Assert.assertEquals(mockNumfs,fsstatSpy.queryFsstat(null,0,0));
-            Assert.assertEquals(mockFs[0],fsstatSpy.getFileSystems(mockNumfs)[0]);
+            try(MockedStatic<Fsstat> fsstatMock = Mockito.mockStatic(Fsstat.class,Mockito.CALLS_REAL_METHODS)){
+                fsstatMock.when(() -> { Fsstat.queryFsstat(null,0,0); })
+                    .thenReturn(mockNumfs);
+                fsstatMock.when(() -> { Fsstat.getFileSystems(mockNumfs);})
+                    .thenReturn(mockFileSystems);
+
+                Assert.assertEquals(Fsstat.queryFsstat(null,0,0),mockNumfs);
+                Assert.assertEquals(Fsstat.getFileSystems(mockNumfs)[0],mockFileSystems[0]);
+
+                Assert.assertEquals(Fsstat.queryPartitionToMountMap(), expeectedMountPointMap);
+            }
         }
     }
 
