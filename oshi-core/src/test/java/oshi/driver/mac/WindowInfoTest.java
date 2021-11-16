11e63179f5ec14b84834b674019cb099f6720e69
hkimbrough22 <haustin.kimbrough@hotmail.com>
2021-11-16 17:01:22 -0500

    adds 4 tests for window layers, removes testing of screensaver layer, and removes testing for windowId

--- oshi-core/src/test/java/oshi/driver/mac/WindowInfoTest.java
+++ oshi-core/src/test/java/oshi/driver/mac/WindowInfoTest.java
@@ -4,12 +4,15 @@ import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
 import com.sun.jna.Platform;
+import org.hamcrest.Matcher;
 import org.junit.jupiter.api.Test;
 import oshi.software.os.OSDesktopWindow;
 import oshi.software.os.OSProcess;
 
+import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 public class WindowInfoTest {
     @Test
@@ -17,18 +20,23 @@ public class WindowInfoTest {
         if(Platform.isMac()){
             final List<OSDesktopWindow> osDesktopWindows = WindowInfo.queryDesktopWindows(true);
             final List<OSDesktopWindow> allOsDesktopWindows = WindowInfo.queryDesktopWindows(false);
-            HashSet<Long> windowIds = new HashSet<>();
+            final Set<Integer> windowOrders = new HashSet<>();
+            final Set<Long> windowIds = new HashSet<>();
 
             assertThat("Desktop should have at least one window.", osDesktopWindows.size(), is(greaterThan(0)));
             assertThat("The number of all desktop windows should be greater than the number of visible desktop windows", allOsDesktopWindows.size(), is(greaterThan(osDesktopWindows.size())));
             for(OSDesktopWindow window : osDesktopWindows){
-                assertThat("Windows should have a title.", window.getTitle(), is(not(emptyOrNullString())));
-                assertThat("Window IDs should be unique.", window.getWindowId(), is(not(in(windowIds))));
+                windowOrders.add(window.getOrder());
                 windowIds.add(window.getWindowId());
-                assertThat("Window order number must be greater than or equal to 0", window.getOrder(), is(greaterThanOrEqualTo(0)));
-                assertThat("Window order number must be less than the number of windows", window.getOrder(), is(lessThan(osDesktopWindows.size())));
+                assertThat("Windows should have a title.", window.getTitle(), is(not(emptyOrNullString())));
                 assertThat("Window should be visible", window.isVisible(), is(true));
+                System.out.println(window);
             }
+            assertThat("Number of layers must be less than or equal to 20.", windowOrders.size(), is(lessThanOrEqualTo(20)));
+            assertThat("Desktop window layer is not present.", windowOrders.contains(0), is(true));
+            assertThat("Dock window layer is not present", windowOrders.contains(24), is(true));
+            assertThat("Dock icon window layer is not present", windowOrders.contains(25), is(true));
+
         }
     }
 }
