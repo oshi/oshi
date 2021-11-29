b818d632fd3c341454dbe16571e10484c9edcdf8
Daniel Widdis <widdis@gmail.com>
2021-11-29 00:17:53 -0800

    Codacy says I can do better at bash

--- config/mergechecks.sh
+++ config/mergechecks.sh
@@ -24,15 +24,15 @@
 #
 
 
-count=$(cat sun_checks.xml | wc -l)
-head -n "$(($count-3))" sun_checks.xml > checkstyle-merged.xml
+count=$(wc -l < sun_checks.xml)
+head -n "$((count-3))" sun_checks.xml > checkstyle-merged.xml
 
 printf "\n    <!-- End sun_checks.xml -->\n\n" >> checkstyle-merged.xml
 
-count=$(cat google_checks.xml | wc -l)
-tail -n "$(($count-49))" google_checks.xml | head -n "$(($count-51))" >> checkstyle-merged.xml
+count=$(wc -l < google_checks.xml)
+tail -n "$((count-49))" google_checks.xml | head -n "$((count-51))" >> checkstyle-merged.xml
 
 printf "\n    <!-- End google_checks.xml -->\n\n" >> checkstyle-merged.xml
 
-count=$(cat custom_checks.xml | wc -l)
-tail -n "$(($count-6))" custom_checks.xml >> checkstyle-merged.xml
+count=$(wc -l < custom_checks.xml)
+tail -n "$((count-6))" custom_checks.xml >> checkstyle-merged.xml
