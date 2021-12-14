03b3f8c3b281fc76317d469580356694eba94d5c
Daniel Widdis <widdis@gmail.com>
2021-12-14 13:40:36 -0800

    Add tests for all WMI drivers and fix failures (#1812)

--- oshi-core/src/main/java/oshi/util/platform/windows/WmiQueryHandler.java
+++ oshi-core/src/main/java/oshi/util/platform/windows/WmiQueryHandler.java
@@ -176,9 +176,10 @@ public class WmiQueryHandler {
             }
         } catch (TimeoutException e) {
             LOG.warn("WMI query timed out after {} ms: {}", wmiTimeout, WmiUtil.queryToString(query));
-        }
-        if (comInit) {
-            unInitCOM();
+        } finally {
+            if (comInit) {
+                unInitCOM();
+            }
         }
         return result;
     }
@@ -195,7 +196,7 @@ public class WmiQueryHandler {
     protected void handleComException(WbemcliUtil.WmiQuery<?> query, COMException ex) {
         LOG.warn(
                 "COM exception querying {}, which might not be on your system. Will not attempt to query it again. Error was {}: {}",
-                query.getWmiClassName(), ex.getHresult().intValue(), ex.getMessage());
+                query.getWmiClassName(), ex.getHresult() == null ? null : ex.getHresult().intValue(), ex.getMessage());
     }
 
     /**
