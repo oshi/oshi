f85464626a1427a87caa016ed8b0fbeb1d371bae
Daniel Widdis <widdis@gmail.com>
2021-12-14 16:56:00 -0800

    JDK 11 changes from master

---
+++ oshi-core/src/main/java/module-info.java
@@ -0,0 +1,24 @@
+module com.github.oshi {
+    // API
+    exports oshi;
+    exports oshi.hardware;
+    exports oshi.software.os;
+    exports oshi.util;
+
+    // JNA needs reflective access to Structure subclasses
+    opens oshi.jna.platform.linux to com.sun.jna;
+    opens oshi.jna.platform.mac to com.sun.jna;
+    opens oshi.jna.platform.windows to com.sun.jna;
+    opens oshi.jna.platform.unix to com.sun.jna;
+    opens oshi.jna.platform.unix.aix to com.sun.jna;
+    opens oshi.jna.platform.unix.freebsd to com.sun.jna;
+    opens oshi.jna.platform.unix.openbsd to com.sun.jna;
+    opens oshi.jna.platform.unix.solaris to com.sun.jna;
+    opens oshi.util.platform.mac to com.sun.jna;
+
+    // dependencies
+    requires com.sun.jna;
+    requires com.sun.jna.platform;
+    requires transitive java.desktop;
+    requires org.slf4j;
+}
