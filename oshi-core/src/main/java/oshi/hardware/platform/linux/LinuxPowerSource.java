d1e894a29651f0c95fc33e81b6db5208a5fb9fc7
Daniel Widdis <widdis@gmail.com>
2022-01-18 16:18:00 -0800

    Fix battery power usage rate on Fedora/RHEL

--- oshi-core/src/main/java/oshi/hardware/platform/linux/LinuxPowerSource.java
+++ oshi-core/src/main/java/oshi/hardware/platform/linux/LinuxPowerSource.java
@@ -1,7 +1,7 @@
 /*
  * MIT License
  *
- * Copyright (c) 2019-2021 The OSHI Project Contributors: https://github.com/oshi/oshi/graphs/contributors
+ * Copyright (c) 2020-2022 The OSHI Project Contributors: https://github.com/oshi/oshi/graphs/contributors
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
@@ -26,15 +26,18 @@ package oshi.hardware.platform.linux;
 import java.io.File;
 import java.time.LocalDate;
 import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+
+import com.sun.jna.platform.linux.Udev; // NOSONAR squid:s1191
+import com.sun.jna.platform.linux.Udev.UdevContext;
+import com.sun.jna.platform.linux.Udev.UdevDevice;
+import com.sun.jna.platform.linux.Udev.UdevEnumerate;
+import com.sun.jna.platform.linux.Udev.UdevListEntry;
 
 import oshi.annotation.concurrent.ThreadSafe;
 import oshi.hardware.PowerSource;
 import oshi.hardware.common.AbstractPowerSource;
 import oshi.util.Constants;
-import oshi.util.FileUtil;
 import oshi.util.ParseUtil;
 
 /**
@@ -42,8 +45,6 @@ import oshi.util.ParseUtil;
  */
 @ThreadSafe
 public final class LinuxPowerSource extends AbstractPowerSource {
-
-    private static final String PS_PATH = "/sys/class/power_supply/";
 
     public LinuxPowerSource(String psName, String psDeviceName, double psRemainingCapacityPercent,
             double psTimeRemainingEstimated, double psTimeRemainingInstant, double psPowerUsageRate, double psVoltage,
@@ -85,78 +86,102 @@ public final class LinuxPowerSource extends AbstractPowerSource {
         String psSerialNumber;
         double psTemperature = 0d;
 
-        // Get list of power source names
-        File f = new File(PS_PATH);
-        String[] psNames = f.list();
         List<PowerSource> psList = new ArrayList<>();
-        // Empty directory will give null rather than empty array, so fix
-        if (psNames != null) {
-            // For each power source, output various info
-            for (String name : psNames) {
-                // Skip if name is ADP* or AC* (AC power supply)
-                if (!name.startsWith("ADP") && !name.startsWith("AC")) {
-                    // Skip if can't read uevent file
-                    List<String> psInfo;
-                    psInfo = FileUtil.readFile(PS_PATH + name + "/uevent", false);
-                    if (psInfo.isEmpty()) {
-                        continue;
-                    }
-                    Map<String, String> psMap = new HashMap<>();
-                    for (String line : psInfo) {
-                        String[] split = line.split("=");
-                        if (split.length > 1 && !split[1].isEmpty()) {
-                            psMap.put(split[0], split[1]);
+        UdevContext udev = Udev.INSTANCE.udev_new();
+        try {
+            UdevEnumerate enumerate = udev.enumerateNew();
+            try {
+                enumerate.addMatchSubsystem("power_supply");
+                enumerate.scanDevices();
+                for (UdevListEntry entry = enumerate.getListEntry(); entry != null; entry = entry.getNext()) {
+                    String syspath = entry.getName();
+                    String name = syspath.substring(syspath.lastIndexOf(File.separatorChar) + 1);
+                    if (!name.startsWith("ADP") && !name.startsWith("AC")) {
+                        UdevDevice device = udev.deviceNewFromSyspath(syspath);
+                        if (device != null) {
+                            try {
+                                if (ParseUtil.parseIntOrDefault(device.getPropertyValue("POWER_SUPPLY_PRESENT"),
+                                        1) > 0) {
+                                    psName = getOrDefault(device, "POWER_SUPPLY_NAME", name);
+                                    String status = device.getPropertyValue("POWER_SUPPLY_STATUS");
+                                    psCharging = "Charging".equals(status);
+                                    psDischarging = "Discharging".equals(status);
+                                    psRemainingCapacityPercent = ParseUtil.parseIntOrDefault(
+                                            device.getPropertyValue("POWER_SUPPLY_CAPACITY"), -100) / 100d;
+
+                                    // Debian/Ubuntu provides Energy. Fedora/RHEL provides Charge.
+                                    psCurrentCapacity = ParseUtil
+                                            .parseIntOrDefault(device.getPropertyValue("POWER_SUPPLY_ENERGY_NOW"), -1);
+                                    if (psCurrentCapacity < 0) {
+                                        psCurrentCapacity = ParseUtil.parseIntOrDefault(
+                                                device.getPropertyValue("POWER_SUPPLY_CHARGE_NOW"), -1);
+                                    }
+                                    psMaxCapacity = ParseUtil
+                                            .parseIntOrDefault(device.getPropertyValue("POWER_SUPPLY_ENERGY_FULL"), 1);
+                                    if (psMaxCapacity < 0) {
+                                        psMaxCapacity = ParseUtil.parseIntOrDefault(
+                                                device.getPropertyValue("POWER_SUPPLY_CHARGE_FULL"), 1);
+                                    }
+                                    psDesignCapacity = ParseUtil.parseIntOrDefault(
+                                            device.getPropertyValue("POWER_SUPPLY_ENERGY_FULL_DESIGN"), 1);
+                                    if (psDesignCapacity < 0) {
+                                        psDesignCapacity = ParseUtil.parseIntOrDefault(
+                                                device.getPropertyValue("POWER_SUPPLY_CHARGE_FULL_DESIGN"), 1);
+                                    }
+
+                                    // Debian/Ubuntu provides Voltage and Power.
+                                    // Fedora/RHEL provides Voltage and Current.
+                                    psVoltage = ParseUtil
+                                            .parseIntOrDefault(device.getPropertyValue("POWER_SUPPLY_VOLTAGE_NOW"), -1);
+                                    // From Physics we know P = IV so I = P/V
+                                    if (psVoltage > 0) {
+                                        String power = device.getPropertyValue("POWER_SUPPLY_POWER_NOW");
+                                        String current = device.getPropertyValue("POWER_SUPPLY_CURRENT_NOW");
+                                        if (power == null) {
+                                            psAmperage = ParseUtil.parseIntOrDefault(current, 0);
+                                            psPowerUsageRate = psAmperage * psVoltage;
+                                        } else if (current == null) {
+                                            psPowerUsageRate = ParseUtil.parseIntOrDefault(power, 0);
+                                            psAmperage = psPowerUsageRate / psVoltage;
+                                        } else {
+                                            psAmperage = ParseUtil.parseIntOrDefault(current, 0);
+                                            psPowerUsageRate = ParseUtil.parseIntOrDefault(power, 0);
+                                        }
+                                    }
+
+                                    psCycleCount = ParseUtil
+                                            .parseIntOrDefault(device.getPropertyValue("POWER_SUPPLY_CYCLE_COUNT"), -1);
+                                    psChemistry = getOrDefault(device, "POWER_SUPPLY_TECHNOLOGY", Constants.UNKNOWN);
+                                    psDeviceName = getOrDefault(device, "POWER_SUPPLY_MODEL_NAME", Constants.UNKNOWN);
+                                    psManufacturer = getOrDefault(device, "POWER_SUPPLY_MANUFACTURER",
+                                            Constants.UNKNOWN);
+                                    psSerialNumber = getOrDefault(device, "POWER_SUPPLY_SERIAL_NUMBER",
+                                            Constants.UNKNOWN);
+
+                                    psList.add(new LinuxPowerSource(psName, psDeviceName, psRemainingCapacityPercent,
+                                            psTimeRemainingEstimated, psTimeRemainingInstant, psPowerUsageRate,
+                                            psVoltage, psAmperage, psPowerOnLine, psCharging, psDischarging,
+                                            psCapacityUnits, psCurrentCapacity, psMaxCapacity, psDesignCapacity,
+                                            psCycleCount, psChemistry, psManufactureDate, psManufacturer,
+                                            psSerialNumber, psTemperature));
+                                }
+                            } finally {
+                                device.unref();
+                            }
                         }
                     }
-                    psName = psMap.getOrDefault("POWER_SUPPLY_NAME", name);
-                    String status = psMap.get("POWER_SUPPLY_STATUS");
-                    psCharging = "Charging".equals(status);
-                    psDischarging = "Discharging".equals(status);
-                    if (psMap.containsKey("POWER_SUPPLY_CAPACITY")) {
-                        psRemainingCapacityPercent = ParseUtil.parseIntOrDefault(psMap.get("POWER_SUPPLY_CAPACITY"),
-                                -100) / 100d;
-                    }
-                    if (psMap.containsKey("POWER_SUPPLY_ENERGY_NOW")) {
-                        psCurrentCapacity = ParseUtil.parseIntOrDefault(psMap.get("POWER_SUPPLY_ENERGY_NOW"), -1);
-                    } else if (psMap.containsKey("POWER_SUPPLY_CHARGE_NOW")) {
-                        psCurrentCapacity = ParseUtil.parseIntOrDefault(psMap.get("POWER_SUPPLY_CHARGE_NOW"), -1);
-                    }
-                    if (psMap.containsKey("POWER_SUPPLY_ENERGY_FULL")) {
-                        psCurrentCapacity = ParseUtil.parseIntOrDefault(psMap.get("POWER_SUPPLY_ENERGY_FULL"), 1);
-                    } else if (psMap.containsKey("POWER_SUPPLY_CHARGE_FULL")) {
-                        psCurrentCapacity = ParseUtil.parseIntOrDefault(psMap.get("POWER_SUPPLY_CHARGE_FULL"), 1);
-                    }
-                    if (psMap.containsKey("POWER_SUPPLY_ENERGY_FULL_DESIGN")) {
-                        psMaxCapacity = ParseUtil.parseIntOrDefault(psMap.get("POWER_SUPPLY_ENERGY_FULL_DESIGN"), 1);
-                    } else if (psMap.containsKey("POWER_SUPPLY_CHARGE_FULL_DESIGN")) {
-                        psMaxCapacity = ParseUtil.parseIntOrDefault(psMap.get("POWER_SUPPLY_CHARGE_FULL_DESIGN"), 1);
-                    }
-                    if (psMap.containsKey("POWER_SUPPLY_VOLTAGE_NOW")) {
-                        psVoltage = ParseUtil.parseIntOrDefault(psMap.get("POWER_SUPPLY_VOLTAGE_NOW"), -1);
-                    }
-                    if (psMap.containsKey("POWER_SUPPLY_POWER_NOW")) {
-                        psPowerUsageRate = ParseUtil.parseIntOrDefault(psMap.get("POWER_SUPPLY_POWER_NOW"), -1);
-                    }
-                    if (psVoltage > 0) {
-                        psAmperage = psPowerUsageRate / psVoltage;
-                    }
-                    if (psMap.containsKey("POWER_SUPPLY_CYCLE_COUNT")) {
-                        psCycleCount = ParseUtil.parseIntOrDefault(psMap.get("POWER_SUPPLY_CYCLE_COUNT"), -1);
-                    }
-                    psChemistry = psMap.getOrDefault("POWER_SUPPLY_TECHNOLOGY", Constants.UNKNOWN);
-                    psDeviceName = psMap.getOrDefault("POWER_SUPPLY_MODEL_NAME", Constants.UNKNOWN);
-                    psManufacturer = psMap.getOrDefault("POWER_SUPPLY_MANUFACTURER", Constants.UNKNOWN);
-                    psSerialNumber = psMap.getOrDefault("POWER_SUPPLY_SERIAL_NUMBER", Constants.UNKNOWN);
-                    if (ParseUtil.parseIntOrDefault(psMap.get("POWER_SUPPLY_PRESENT"), 1) > 0) {
-                        psList.add(new LinuxPowerSource(psName, psDeviceName, psRemainingCapacityPercent,
-                                psTimeRemainingEstimated, psTimeRemainingInstant, psPowerUsageRate, psVoltage,
-                                psAmperage, psPowerOnLine, psCharging, psDischarging, psCapacityUnits,
-                                psCurrentCapacity, psMaxCapacity, psDesignCapacity, psCycleCount, psChemistry,
-                                psManufactureDate, psManufacturer, psSerialNumber, psTemperature));
-                    }
                 }
+            } finally {
+                enumerate.unref();
             }
+        } finally {
+            udev.unref();
         }
         return psList;
+    }
+
+    private static String getOrDefault(UdevDevice device, String property, String def) {
+        String value = device.getPropertyValue(property);
+        return value == null ? def : value;
     }
 }
