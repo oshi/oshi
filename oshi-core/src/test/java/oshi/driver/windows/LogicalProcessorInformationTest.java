ab1a64188214b0a73e289988dab4836e3e0cb436
Daniel Widdis <widdis@gmail.com>
2022-02-07 10:52:47 -0800

    Add Logical Processor Information Test

---
+++ oshi-core/src/test/java/oshi/driver/windows/LogicalProcessorInformationTest.java
@@ -0,0 +1,56 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2022 The OSHI Project Contributors: https://github.com/oshi/oshi/graphs/contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package oshi.driver.windows;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThanOrEqualTo;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.OS;
+
+import oshi.hardware.CentralProcessor.LogicalProcessor;
+import oshi.hardware.CentralProcessor.PhysicalProcessor;
+import oshi.util.tuples.Pair;
+
+@EnabledOnOs(OS.WINDOWS)
+class LogicalProcessorInformationTest {
+    @Test
+    void testGetLogicalProcessorInformation() {
+        Pair<List<LogicalProcessor>, List<PhysicalProcessor>> info = LogicalProcessorInformation
+                .getLogicalProcessorInformation();
+        assertThat("Must be more Logical Processors than Physical Ones", info.getA().size(),
+                greaterThanOrEqualTo(info.getB().size()));
+    }
+
+    @Test
+    void testGetLogicalProcessorInformationEx() {
+        Pair<List<LogicalProcessor>, List<PhysicalProcessor>> info = LogicalProcessorInformation
+                .getLogicalProcessorInformationEx();
+        assertThat("Must be more Logical Processors than Physical Ones", info.getA().size(),
+                greaterThanOrEqualTo(info.getB().size()));
+    }
+}
