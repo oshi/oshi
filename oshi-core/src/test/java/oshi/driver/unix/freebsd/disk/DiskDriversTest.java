872c5eddfd76d79b616b751456cbdeb0068fc318
Daniel Widdis <widdis@gmail.com>
2022-02-12 11:33:50 -0800

    FreeBSD and OpenBSD Disk Driver Tests (#1916)

---
+++ oshi-core/src/test/java/oshi/driver/unix/freebsd/disk/DiskDriversTest.java
@@ -0,0 +1,75 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2022 The OSHI Project Contributors: https://github.com/oshi/oshi/graphs/contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package oshi.driver.unix.freebsd.disk;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.anEmptyMap;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.not;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+import java.util.Map;
+import java.util.Map.Entry;
+import java.util.Set;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.OS;
+
+import com.sun.jna.Platform; // NOSONAR squid:S1191
+
+import oshi.hardware.HWPartition;
+import oshi.util.tuples.Triplet;
+
+@EnabledOnOs(OS.OTHER)
+class DiskDriversTest {
+    @Test
+    void testDiskDrivers() {
+        if (Platform.isFreeBSD()) {
+            Map<String, Triplet<String, String, Long>> diskStringMap = GeomDiskList.queryDisks();
+            assertThat("Disk string map should not be empty", diskStringMap, not(anEmptyMap()));
+
+            Map<String, List<HWPartition>> partMap = GeomPartList.queryPartitions();
+            assertThat("Partition map should not be empty", partMap, not(anEmptyMap()));
+
+            Map<String, String> deviceMap = Mount.queryPartitionToMountMap();
+            assertThat("Partition to mount map should not be empty", deviceMap, not(anEmptyMap()));
+
+            Set<String> diskNames = diskStringMap.keySet();
+            Set<String> mountedPartitions = deviceMap.keySet();
+            int mounted = 0;
+            for (Entry<String, List<HWPartition>> entry : partMap.entrySet()) {
+                assertTrue(diskNames.contains(entry.getKey()), "Disk name should be in disk string map");
+                for (HWPartition part : entry.getValue()) {
+                    if (!part.getMountPoint().isEmpty()) {
+                        assertTrue(mountedPartitions.contains(entry.getKey()), "Partition name should be in mount map");
+                        mounted++;
+                    }
+                }
+            }
+            assertThat("At least one partition is mounted", mounted, greaterThan(0));
+        }
+    }
+}
---
+++ oshi-core/src/test/java/oshi/driver/unix/openbsd/disk/DisklabelTest.java
@@ -0,0 +1,66 @@
+/*
+ * MIT License
+ *
+ * Copyright (c) 2022 The OSHI Project Contributors: https://github.com/oshi/oshi/graphs/contributors
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in all
+ * copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+ * SOFTWARE.
+ */
+package oshi.driver.unix.openbsd.disk;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.nullValue;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.OS;
+
+import com.sun.jna.Platform; // NOSONAR squid:S1191
+
+import oshi.hardware.HWPartition;
+import oshi.software.os.unix.openbsd.OpenBsdOperatingSystem;
+import oshi.util.platform.unix.openbsd.OpenBsdSysctlUtil;
+import oshi.util.tuples.Quartet;
+
+@EnabledOnOs(OS.OTHER)
+class DisklabelTest {
+    @Test
+    void testDisklabel() {
+        if (Platform.isOpenBSD()) {
+            String[] devices = OpenBsdSysctlUtil.sysctl("hw.disknames", "").split(",");
+            for (String device : devices) {
+                String diskName = device.split(":")[0];
+                Quartet<String, String, Long, List<HWPartition>> diskdata = Disklabel.getDiskParams(diskName);
+                // First 3 only available with elevation
+                if (new OpenBsdOperatingSystem().isElevated()) {
+                    assertThat("Disk label is not null", diskdata.getA(), not(nullValue()));
+                    assertThat("Disk duid is not null", diskdata.getB(), not(nullValue()));
+                    assertThat("Disk size is nonzero", diskdata.getC().longValue(), greaterThan(0L));
+                }
+                for (HWPartition part : diskdata.getD()) {
+                    assertTrue(part.getName().startsWith(diskName), "Partition name should start with disk name");
+                }
+            }
+        }
+    }
+}
