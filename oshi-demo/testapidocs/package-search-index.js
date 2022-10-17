492e1aa4e6ec02244567dc0be8a0a2f086926208
dbwiddis <widdis@gmail.com>
2022-10-17 10:30:58 +0000

    Deploying to gh-pages from @ oshi/oshi@f8f336d9ed82470f15dba20f85bf173f6526127d 🚀

--- oshi-demo/testapidocs/package-search-index.js
+++ oshi-demo/testapidocs/package-search-index.js
@@ -1,1 +1,1 @@
-packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"oshi.demo"},{"l":"oshi.demo.gui"}];updateSearchResults();
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html"},{"l":"oshi.demo"},{"l":"oshi.demo.gui"},{"l":"oshi.demo.jmx"},{"l":"oshi.demo.jmx.api"},{"l":"oshi.demo.jmx.demo"},{"l":"oshi.demo.jmx.mbeans"},{"l":"oshi.demo.jmx.strategiesplatform"}];updateSearchResults();
