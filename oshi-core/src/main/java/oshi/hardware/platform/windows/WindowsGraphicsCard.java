af028d6dfa0546aba1755f867bf9572d09a48793
Sergey Yungman <komelgman@yandex.ru>
2023-12-05 14:22:16 +0300

    fix: Wrong VRam value #2521 - read GraphicCard info from registry

--- oshi-core/src/main/java/oshi/hardware/platform/windows/WindowsGraphicsCard.java
+++ oshi-core/src/main/java/oshi/hardware/platform/windows/WindowsGraphicsCard.java
@@ -7,8 +7,15 @@ package oshi.hardware.platform.windows;
 import java.util.ArrayList;
 import java.util.List;
 
-import com.sun.jna.platform.win32.VersionHelpers;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.sun.jna.platform.win32.Advapi32Util;
 import com.sun.jna.platform.win32.COM.WbemcliUtil.WmiResult;
+import com.sun.jna.platform.win32.VersionHelpers;
+import com.sun.jna.platform.win32.Win32Exception;
+import com.sun.jna.platform.win32.WinError;
+import com.sun.jna.platform.win32.WinReg;
 
 import oshi.annotation.concurrent.Immutable;
 import oshi.driver.windows.wmi.Win32VideoController;
@@ -27,7 +34,17 @@ import oshi.util.tuples.Triplet;
 @Immutable
 final class WindowsGraphicsCard extends AbstractGraphicsCard {
 
+    private static final Logger LOG = LoggerFactory.getLogger(WindowsGraphicsCard.class);
+
     private static final boolean IS_VISTA_OR_GREATER = VersionHelpers.IsWindowsVistaOrGreater();
+    public static final String ADAPTER_STRING = "HardwareInformation.AdapterString";
+    public static final String DRIVER_DESC = "DriverDesc";
+    public static final String DRIVER_VERSION = "DriverVersion";
+    public static final String VENDOR = "ProviderName";
+    public static final String QW_MEMORY_SIZE = "HardwareInformation.qwMemorySize";
+    public static final String MEMORY_SIZE = "HardwareInformation.MemorySize";
+    public static final String DISPLAY_DEVICES_REGISTRY_PATH =
+        "SYSTEM\\CurrentControlSet\\Control\\Class\\{4d36e968-e325-11ce-bfc1-08002be10318}\\";
 
     /**
      * Constructor for WindowsGraphicsCard
@@ -48,6 +65,79 @@ final class WindowsGraphicsCard extends AbstractGraphicsCard {
      * @return List of {@link oshi.hardware.platform.windows.WindowsGraphicsCard} objects.
      */
     public static List<GraphicsCard> getGraphicsCards() {
+        List<GraphicsCard> cardList = new ArrayList<>();
+
+        String[] keys = Advapi32Util.registryGetKeys(WinReg.HKEY_LOCAL_MACHINE, DISPLAY_DEVICES_REGISTRY_PATH);
+        for (String key : keys) {
+            if (!key.startsWith("0")) {
+                continue;
+            }
+
+            try {
+                String fullKey = DISPLAY_DEVICES_REGISTRY_PATH + key;
+                if (!Advapi32Util.registryValueExists(WinReg.HKEY_LOCAL_MACHINE, fullKey, ADAPTER_STRING)) {
+                    continue;
+                }
+
+                String name = Advapi32Util.registryGetStringValue(WinReg.HKEY_LOCAL_MACHINE, fullKey, DRIVER_DESC);
+                String deviceId = null; // Note: not found proper key in registry
+                String vendor = Advapi32Util.registryGetStringValue(WinReg.HKEY_LOCAL_MACHINE, fullKey, VENDOR);
+                String versionInfo = Advapi32Util.registryGetStringValue(WinReg.HKEY_LOCAL_MACHINE, fullKey, DRIVER_VERSION);
+                long vram = 0L;
+
+                if (Advapi32Util.registryValueExists(WinReg.HKEY_LOCAL_MACHINE, fullKey, QW_MEMORY_SIZE)) {
+                    vram = Advapi32Util.registryGetLongValue(WinReg.HKEY_LOCAL_MACHINE, fullKey, QW_MEMORY_SIZE);
+                } else if (Advapi32Util.registryValueExists(WinReg.HKEY_LOCAL_MACHINE, fullKey, MEMORY_SIZE)) {
+                    Object o = Advapi32Util.registryGetValue(WinReg.HKEY_LOCAL_MACHINE, fullKey, MEMORY_SIZE);
+                    if (o instanceof Long || o instanceof Integer) {
+                        vram = (long) o;
+                    } else if (o instanceof byte[]) {
+                        vram = memSizeBinaryToLong((byte[])o);
+                    }
+                }
+
+                cardList.add(new WindowsGraphicsCard(
+                    Util.isBlank(name) ? Constants.UNKNOWN : name,
+                    Util.isBlank(deviceId) ? Constants.UNKNOWN : deviceId,
+                    Util.isBlank(vendor) ? Constants.UNKNOWN : vendor,
+                    Util.isBlank(versionInfo) ? Constants.UNKNOWN : versionInfo,
+                    vram));
+            } catch (Win32Exception e) {
+                if (e.getErrorCode() != WinError.ERROR_ACCESS_DENIED) {
+                    // Ignore access denied errors, re-throw others
+                    throw e;
+                }
+            }
+        }
+
+        if (cardList.isEmpty()) {
+            return getGraphicsCardsFromWmi();
+        }
+        return cardList;
+    }
+
+    // Note: not found spec, working for my Intel UHD embedded gpu
+    private static long memSizeBinaryToLong(byte[] bytes) {
+        long result = 0;
+
+        if (bytes.length > 8) {
+            LOG.debug("byte[] to long: Oops! bytes length > 8");
+            return 0;
+        }
+
+        for (byte i = 0; i < bytes.length; ++i) {
+            result += (long) (bytes[i] & 255) << 8 * i;
+        }
+
+        return result;
+    }
+
+    /**
+     * public method used by {@link oshi.hardware.common.AbstractHardwareAbstractionLayer} to access the graphics cards.
+     *
+     * @return List of {@link oshi.hardware.platform.windows.WindowsGraphicsCard} objects.
+     */
+    public static List<GraphicsCard> getGraphicsCardsFromWmi() {
         List<GraphicsCard> cardList = new ArrayList<>();
         if (IS_VISTA_OR_GREATER) {
             WmiResult<VideoControllerProperty> cards = Win32VideoController.queryVideoController();
