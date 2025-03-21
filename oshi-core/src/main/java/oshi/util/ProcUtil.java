058cb8c77a2d6df9599481ca14f8ba46b976ed1d
Jens Deppe <jens.deppe@broadcom.com>
2025-03-20 17:35:09 -0700

    Add raw access to map structures in proc filesystem (#2854)
    * Add raw access to /proc/net/snmp, /proc/net/snmp6 and /proc/net/netstat
    
    Provide methods to process and marshall the named /proc files into a
    consistent map structure of { key: { stat: value }}.
    
    * Review update and checkstyle fixes
    
    * Review updates
    
    - Moved method into ProcUtil
    - Removed processNetSnmp6 since FileUtils.getKeyValueMapFromFile should
      suffice.
    
    * Don't run test on Windows
    
    * More review updates
    
    * Missed some review requests
    
    * Rename and move test class to avoid module access issues
    
    * More cleanup. Add an overloaded parseStatistics method with default whitespace separator.
    
    * Add checks for malformed parsed lines
    
    * Update CHANGELOG

---
+++ oshi-core/src/main/java/oshi/util/ProcUtil.java
@@ -0,0 +1,145 @@
+package oshi.util;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.regex.Pattern;
+
+import oshi.annotation.concurrent.ThreadSafe;
+
+@ThreadSafe
+public final class ProcUtil {
+
+    private ProcUtil() {
+    }
+
+    /**
+     * Parses /proc files with a given structure consisting of a keyed header line followed by a
+     * keyed value line. {@code /proc/net/netstat} and {@code /proc/net/snmp} are specific examples
+     * of this. The returned map is of the form {key: {stat: value, stat: value, ...}}. An example
+     * of the file structure is:
+     * <pre>
+     *     TcpExt: SyncookiesSent SyncookiesRecv SyncookiesFailed ...
+     *     TcpExt: 0 4 0 ...
+     *     IpExt: InNoRoutes InTruncatedPkts InMcastPkts OutMcastPkts ...
+     *     IpExt: 55 0 27786 1435 ...
+     *     MPTcpExt: MPCapableSYNRX MPCapableSYNTX MPCapableSYNACKRX ...
+     *     MPTcpExt: 0 0 0 ...
+     * </pre>
+     * Which would produce a mapping structure like:
+     * <pre>
+     *     {
+     *         "TcpExt": {"SyncookiesSent":0, "SyncookiesRecv":4, "SyncookiesFailed":0, ... }
+     *         "IpExt": {"InNoRoutes":55, "InTruncatedPkts":27786, "InMcastPkts":1435, ... }
+     *         "MPTcpExt": {"MPCapableSYNACKRX":0, "MPCapableSYNTX":0, "MPCapableSYNACKRX":0, ... }
+     *     }
+     * </pre>
+     *
+     * @param procFile the file to process
+     * @param keys an optional array of keys to return in the outer map. If none are given, all
+     *             found keys are returned.
+     * @return a map of keys to stats
+     */
+    public static Map<String, Map<String, Long>> parseNestedStatistics(String procFile, String... keys) {
+        Map<String, Map<String, Long>> result = new HashMap<>();
+        List<String> keyList = Arrays.asList(keys);
+
+        List<String> lines = FileUtil.readFile(procFile);
+        String previousKey = null;
+        String[] statNames = null;
+
+        for (String line : lines) {
+            String[] parts = ParseUtil.whitespaces.split(line);
+            if (parts.length == 0) {
+                continue;
+            }
+
+            // This would happen if the line starts with whitespace
+            if (parts[0].isEmpty()) {
+                parts = Arrays.copyOfRange(parts, 1, parts.length);
+            }
+
+            String key = parts[0].substring(0, parts[0].length() - 1);
+
+            if (!keyList.isEmpty() && !keyList.contains(key)) {
+                continue;
+            }
+
+            if (key.equals(previousKey)) {
+                if (parts.length == statNames.length) {
+                    Map<String, Long> stats = new HashMap<>(parts.length - 1);
+                    for (int i = 1; i < parts.length; i++) {
+                        stats.put(statNames[i], ParseUtil.parseLongOrDefault(parts[i], 0));
+                    }
+                    result.put(key, stats);
+                }
+            } else {
+                statNames = parts;
+            }
+
+            previousKey = key;
+        }
+
+        return result;
+    }
+
+    /**
+     * Parses /proc files formatted as "statistic  (long)value" to produce a simple mapping. An
+     * example would be /proc/net/snmp6. The file format would look like:
+     * <pre>
+     *    Ip6InReceives             8026
+     *    Ip6InHdrErrors            0
+     *    Icmp6InMsgs               2
+     *    Icmp6InErrors             0
+     *    Icmp6OutMsgs              424
+     *    Udp6IgnoredMulti          5
+     *    Udp6MemErrors             1
+     *    UdpLite6InDatagrams       37
+     *    UdpLite6NoPorts           1
+     * </pre>
+     * Which would produce a mapping structure like:
+     * <pre>
+     *     {
+     *         "Ip6InReceives":8026,
+     *         "Ip6InHdrErrors":0,
+     *         "Icmp6InMsgs":2,
+     *         "Icmp6InErrors":0,
+     *         ...
+     *     }
+     * </pre>
+     *
+     * @param procFile the file to process
+     * @param separator a regex specifying the separator between statistic and value
+     * @return a map of statistics and associated values
+     */
+    public static Map<String, Long> parseStatistics(String procFile, Pattern separator) {
+        Map<String, Long> result = new HashMap<>();
+        List<String> lines = FileUtil.readFile(procFile);
+        for (String line : lines) {
+            String[] parts = separator.split(line);
+
+            // This would happen if the line starts with the given separator (whitespace?)
+            if (parts[0].isEmpty()) {
+                parts = Arrays.copyOfRange(parts, 1, parts.length);
+            }
+
+            if (parts.length == 2) {
+                result.put(parts[0], ParseUtil.parseLongOrDefault(parts[1], 0));
+            }
+        }
+
+        return result;
+    }
+
+    /**
+     * Overloaded {@link #parseStatistics(String, Pattern)} using a whitespace separator.
+     *
+     * @param procFile the file to process
+     * @return a map of statistics and associated values
+     * @see #parseStatistics(String, Pattern)
+     */
+    public static Map<String, Long> parseStatistics(String procFile) {
+        return parseStatistics(procFile, ParseUtil.whitespaces);
+    }
+}
---
+++ oshi-core/src/test/java/oshi/util/ProcUtilsTest.java
@@ -0,0 +1,72 @@
+package oshi.util;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.contains;
+import static org.hamcrest.Matchers.containsInAnyOrder;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+
+import java.util.Map;
+
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.condition.EnabledOnOs;
+import org.junit.jupiter.api.condition.OS;
+
+@EnabledOnOs(OS.LINUX)
+public class ProcUtilsTest {
+
+    @Test
+    void testRawNetNetstat() {
+        String resource =
+            ProcUtilsTest.class.getResource("sample-proc-net-netstat.txt").getFile();
+
+        Map<String, Map<String, Long>> results = ProcUtil.parseNestedStatistics(resource);
+
+        assertThat(results.keySet(), containsInAnyOrder("TcpExt", "IpExt", "MPTcpExt", "BadExt", "MoreBadExt"));
+        assertThat(results.get("TcpExt").get("SyncookiesSent"), is(6L));
+        assertThat(results.get("TcpExt").get("TCPAODroppedIcmps"), is(3L));
+        assertThat(results.get("IpExt").get("InNoRoutes"), is(55L));
+        assertThat(results.get("MPTcpExt").get("MPCurrEstab"), is(1L));
+        assertThat(results.get("BadExt").get("One"), is(1L));
+        assertThat(results.get("MoreBadExt").get("Six"), is(6L));
+    }
+
+    @Test
+    void testRawNetNetstatWithLimitedKeys() {
+        String resource =
+            ProcUtilsTest.class.getResource("sample-proc-net-netstat.txt").getFile();
+
+        Map<String, Map<String, Long>> results = ProcUtil.parseNestedStatistics(resource, "IpExt");
+
+        assertThat(results.keySet(), contains("IpExt"));
+        assertThat(results.get("IpExt").get("InNoRoutes"), is(55L));
+    }
+
+    @Test
+    void testRawNetSnmp() {
+        String resource =
+            ProcUtilsTest.class.getResource("sample-proc-net-snmp.txt").getFile();
+
+        Map<String, Map<String, Long>> results = ProcUtil.parseNestedStatistics(resource);
+
+        assertThat(results.keySet(), containsInAnyOrder("Ip", "Icmp", "IcmpMsg", "Tcp", "Udp", "UdpLite"));
+        assertThat(results.get("Tcp").get("ActiveOpens"), is(1892L));
+        assertThat(results.get("Ip").get("OutTransmits"), is(66296L));
+        assertThat(results.get("Icmp").get("InMsgs"), is(184L));
+    }
+
+    @Test
+    void testRawNetSnmp6() {
+        String resource =
+            ProcUtilsTest.class.getResource("sample-proc-net-snmp6.txt").getFile();
+
+        Map<String, Long> results = ProcUtil.parseStatistics(resource);
+
+        assertThat(results.keySet(), hasSize(91));
+        assertThat(results.get("Ip6InReceives"), is(8026L));
+        assertThat(results.get("Ip6OutMcastOctets"), is(45957L));
+        assertThat(results.get("UdpLite6MemErrors"), is(1L));
+        assertThat(results.get("IndentedEntry"), is(37L));
+    }
+
+}
