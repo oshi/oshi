af1b374a66958df52559547e96fa99e444738cae
Daniel Widdis <widdis@gmail.com>
2025-08-23 10:47:50 -0700

    Update pom file copying for consistency, use JDK 24 for testing (#2948)

---
+++ oshi-core-java25/src/main/java/module-info.java
@@ -0,0 +1,23 @@
+/**
+ * This module provides access to the OSHI API and utility functions.
+ */
+module com.github.oshi {
+    // API
+    exports oshi;
+    exports oshi.hardware;
+    exports oshi.software.os;
+    exports oshi.util;
+
+    // JNA needs reflective access to Structure and ByReference subclasses
+    opens oshi.jna to com.sun.jna;
+    opens oshi.jna.platform.linux to com.sun.jna;
+    opens oshi.jna.platform.mac to com.sun.jna;
+    opens oshi.jna.platform.windows to com.sun.jna;
+    opens oshi.jna.platform.unix to com.sun.jna;
+
+    // dependencies
+    requires com.sun.jna;
+    requires com.sun.jna.platform;
+    requires transitive java.desktop;
+    requires org.slf4j;
+}
