69ca571ae45bc4202bd6bebdbf4941bda88d14d4
dbwiddis <widdis@gmail.com>
2026-04-21 05:05:38 +0000

    Deploying to gh-pages from @ oshi/oshi@33e5ada5932ded673db25f2d33082ad52f6a0a0d 🚀

---
+++ oshi-benchmark/apidocs/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html","k":"18"},{"p":"oshi.benchmark","l":"ProcessesBenchmark"}];updateSearchResults();
---
+++ oshi-benchmark/testapidocs/package-search-index.js
@@ -0,0 +1,1 @@
+packageSearchIndex = [{"l":"All Packages","u":"allpackages-index.html","k":"18"},{"l":"oshi.benchmark"}];updateSearchResults();
