fdb52702fc115edf271fc417036516326d696b90
Daniel Widdis <widdis@gmail.com>
2026-06-10 21:47:48 -0700

    Migrate FreeBSD OSProcess onto BsdOSProcess
    FreeBsdOSProcess extends BsdOSProcess; the JNA and FFM concretes drop the
    inherited fields/getters/suppliers/affinity/limits and keep only the
    sysctl arg/env/bitness reads, getrlimit, procstat cwd/open-files, ps thread
    enumeration, and the ps attribute parsing.

--- oshi-core/src/main/java/oshi/software/os/unix/freebsd/FreeBsdOSProcessJNA.java
+++ oshi-core/src/main/java/oshi/software/os/unix/freebsd/FreeBsdOSProcessJNA.java
@@ -5,24 +5,12 @@
 package oshi.software.os.unix.freebsd;
 
 import static oshi.software.os.OSProcess.State.INVALID;
-import static oshi.software.os.OSProcess.State.OTHER;
-import static oshi.software.os.OSProcess.State.RUNNING;
-import static oshi.software.os.OSProcess.State.SLEEPING;
-import static oshi.software.os.OSProcess.State.STOPPED;
-import static oshi.software.os.OSProcess.State.WAITING;
-import static oshi.software.os.OSProcess.State.ZOMBIE;
 import static oshi.software.os.OSThread.ThreadFiltering.VALID_THREAD;
-import static oshi.util.Memoizer.memoize;
 
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.Collections;
 import java.util.List;
-import java.util.Locale;
 import java.util.Map;
-import java.util.Optional;
 import java.util.function.Predicate;
-import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
@@ -41,13 +29,12 @@ import oshi.software.common.os.unix.freebsd.FreeBsdOSThread;
 import oshi.software.os.OSThread;
 import oshi.software.os.unix.freebsd.FreeBsdOperatingSystemJNA.PsKeywords;
 import oshi.util.ExecutingCommand;
-import oshi.util.FileUtil;
 import oshi.util.ParseUtil;
 import oshi.util.common.platform.unix.freebsd.ProcstatUtil;
 import oshi.util.platform.unix.freebsd.BsdSysctlUtil;
 
 /**
- * OSProcess implementation
+ * JNA-backed FreeBSD OSProcess.
  */
 @ThreadSafe
 public class FreeBsdOSProcessJNA extends FreeBsdOSProcess {
@@ -58,35 +45,6 @@ public class FreeBsdOSProcessJNA extends FreeBsdOSProcess {
 
     private final FreeBsdOperatingSystemJNA os;
 
-    private Supplier<Integer> bitness = memoize(this::queryBitness);
-    private Supplier<String> commandLine = memoize(this::queryCommandLine);
-    private Supplier<List<String>> arguments = memoize(this::queryArguments);
-    private Supplier<Map<String, String>> environmentVariables = memoize(this::queryEnvironmentVariables);
-
-    private String name;
-    private String path = "";
-    private String user;
-    private String userID;
-    private String group;
-    private String groupID;
-    private State state = INVALID;
-    private int parentProcessID;
-    private int threadCount;
-    private int priority;
-    private long virtualSize;
-    private long residentSetSize;
-    private long kernelTime;
-    private long userTime;
-    private long startTime;
-    private long upTime;
-    private long bytesRead;
-    private long bytesWritten;
-    private long minorFaults;
-    private long majorFaults;
-    private long voluntaryContextSwitches;
-    private long involuntaryContextSwitches;
-    private String commandLineBackup;
-
     public FreeBsdOSProcessJNA(int pid, Map<PsKeywords, String> psMap, FreeBsdOperatingSystemJNA os) {
         super(pid);
         this.os = os;
@@ -94,31 +52,7 @@ public class FreeBsdOSProcessJNA extends FreeBsdOSProcess {
     }
 
     @Override
-    public String getName() {
-        return this.name;
-    }
-
-    @Override
-    public String getPath() {
-        return this.path;
-    }
-
-    @Override
-    public String getCommandLine() {
-        return this.commandLine.get();
-    }
-
-    private String queryCommandLine() {
-        String cl = String.join(" ", getArguments());
-        return cl.isEmpty() ? this.commandLineBackup : cl;
-    }
-
-    @Override
-    public List<String> getArguments() {
-        return arguments.get();
-    }
-
-    private List<String> queryArguments() {
+    protected List<String> queryArguments() {
         if (ARGMAX > 0) {
             // Get arguments via sysctl(3)
             int[] mib = new int[4];
@@ -144,11 +78,7 @@ public class FreeBsdOSProcessJNA extends FreeBsdOSProcess {
     }
 
     @Override
-    public Map<String, String> getEnvironmentVariables() {
-        return environmentVariables.get();
-    }
-
-    private Map<String, String> queryEnvironmentVariables() {
+    protected Map<String, String> queryEnvironmentVariables() {
         if (ARGMAX > 0) {
             // Get environment variables via sysctl(3)
             int[] mib = new int[4];
@@ -179,86 +109,6 @@ public class FreeBsdOSProcessJNA extends FreeBsdOSProcess {
     }
 
     @Override
-    public String getUser() {
-        return this.user;
-    }
-
-    @Override
-    public String getUserID() {
-        return this.userID;
-    }
-
-    @Override
-    public String getGroup() {
-        return this.group;
-    }
-
-    @Override
-    public String getGroupID() {
-        return this.groupID;
-    }
-
-    @Override
-    public State getState() {
-        return this.state;
-    }
-
-    @Override
-    public int getParentProcessID() {
-        return this.parentProcessID;
-    }
-
-    @Override
-    public int getThreadCount() {
-        return this.threadCount;
-    }
-
-    @Override
-    public int getPriority() {
-        return this.priority;
-    }
-
-    @Override
-    public long getVirtualSize() {
-        return this.virtualSize;
-    }
-
-    @Override
-    public long getResidentMemory() {
-        return this.residentSetSize;
-    }
-
-    @Override
-    public long getKernelTime() {
-        return this.kernelTime;
-    }
-
-    @Override
-    public long getUserTime() {
-        return this.userTime;
-    }
-
-    @Override
-    public long getUpTime() {
-        return this.upTime;
-    }
-
-    @Override
-    public long getStartTime() {
-        return this.startTime;
-    }
-
-    @Override
-    public long getBytesRead() {
-        return this.bytesRead;
-    }
-
-    @Override
-    public long getBytesWritten() {
-        return this.bytesWritten;
-    }
-
-    @Override
     public long getOpenFiles() {
         return ProcstatUtil.getOpenFiles(getProcessID());
     }
@@ -286,33 +136,7 @@ public class FreeBsdOSProcessJNA extends FreeBsdOSProcess {
     }
 
     @Override
-    public int getBitness() {
-        return this.bitness.get();
-    }
-
-    @Override
-    public long getAffinityMask() {
-        long bitMask = 0L;
-        // Would prefer to use native cpuset_getaffinity call but variable sizing is
-        // kernel-dependent and requires C macros, so we use commandline instead.
-        String cpuset = ExecutingCommand.getFirstAnswer("cpuset -gp " + getProcessID());
-        // Sample output:
-        // pid 8 mask: 0, 1
-        // cpuset: getaffinity: No such process
-        String[] split = cpuset.split(":");
-        if (split.length > 1) {
-            String[] bits = split[1].split(",");
-            for (String bit : bits) {
-                int bitToSet = ParseUtil.parseIntOrDefault(bit.trim(), -1);
-                if (bitToSet >= 0 && bitToSet < Long.SIZE) {
-                    bitMask |= 1L << bitToSet;
-                }
-            }
-        }
-        return bitMask;
-    }
-
-    private int queryBitness() {
+    protected int queryBitness() {
         // Get process abi vector
         int[] mib = new int[4];
         mib[0] = 1; // CTL_KERN
@@ -348,26 +172,6 @@ public class FreeBsdOSProcessJNA extends FreeBsdOSProcess {
     }
 
     @Override
-    public long getMinorFaults() {
-        return this.minorFaults;
-    }
-
-    @Override
-    public long getMajorFaults() {
-        return this.majorFaults;
-    }
-
-    @Override
-    public long getVoluntaryContextSwitches() {
-        return this.voluntaryContextSwitches;
-    }
-
-    @Override
-    public long getInvoluntaryContextSwitches() {
-        return this.involuntaryContextSwitches;
-    }
-
-    @Override
     public boolean updateAttributes() {
         String psCommand = "ps -awwxo " + FreeBsdOperatingSystemJNA.PS_COMMAND_ARGS + " -p " + getProcessID();
         List<String> procList = ExecutingCommand.runNative(psCommand);
@@ -385,29 +189,7 @@ public class FreeBsdOSProcessJNA extends FreeBsdOSProcess {
 
     private boolean updateAttributes(Map<PsKeywords, String> psMap) {
         long now = System.currentTimeMillis();
-        switch (psMap.get(PsKeywords.STATE).charAt(0)) {
-            case 'R':
-                this.state = RUNNING;
-                break;
-            case 'I':
-            case 'S':
-                this.state = SLEEPING;
-                break;
-            case 'D':
-            case 'L':
-            case 'U':
-                this.state = WAITING;
-                break;
-            case 'Z':
-                this.state = ZOMBIE;
-                break;
-            case 'T':
-                this.state = STOPPED;
-                break;
-            default:
-                this.state = OTHER;
-                break;
-        }
+        this.state = getStateFromOutput(psMap.get(PsKeywords.STATE).charAt(0));
         this.parentProcessID = ParseUtil.parseIntOrDefault(psMap.get(PsKeywords.PPID), 0);
         this.user = psMap.get(PsKeywords.USER);
         this.userID = psMap.get(PsKeywords.UID);
@@ -428,31 +210,9 @@ public class FreeBsdOSProcessJNA extends FreeBsdOSProcess {
         this.name = this.path.substring(this.path.lastIndexOf('/') + 1);
         this.minorFaults = ParseUtil.parseLongOrDefault(psMap.get(PsKeywords.MINFLT), 0L);
         this.majorFaults = ParseUtil.parseLongOrDefault(psMap.get(PsKeywords.MAJFLT), 0L);
-        long voluntaryContextSwitches = ParseUtil.parseLongOrDefault(psMap.get(PsKeywords.NVCSW), 0L);
-        long nonVoluntaryContextSwitches = ParseUtil.parseLongOrDefault(psMap.get(PsKeywords.NIVCSW), 0L);
-        this.voluntaryContextSwitches = voluntaryContextSwitches;
-        this.involuntaryContextSwitches = nonVoluntaryContextSwitches;
+        this.voluntaryContextSwitches = ParseUtil.parseLongOrDefault(psMap.get(PsKeywords.NVCSW), 0L);
+        this.involuntaryContextSwitches = ParseUtil.parseLongOrDefault(psMap.get(PsKeywords.NIVCSW), 0L);
         this.commandLineBackup = psMap.get(PsKeywords.ARGS);
         return true;
-    }
-
-    private long getProcessOpenFileLimit(long processId, int index) {
-        final String limitsPath = String.format(Locale.ROOT, "/proc/%d/limits", processId);
-        if (!Files.exists(Paths.get(limitsPath))) {
-            return -1; // not supported
-        }
-        final List<String> lines = FileUtil.readFile(limitsPath);
-        final Optional<String> maxOpenFilesLine = lines.stream().filter(line -> line.startsWith("Max open files"))
-                .findFirst();
-        if (!maxOpenFilesLine.isPresent()) {
-            return -1;
-        }
-
-        // Split all non-Digits away -> ["", "{soft-limit}, "{hard-limit}"]
-        final String[] split = maxOpenFilesLine.get().split("\\D+");
-        if (split.length <= index) {
-            return -1;
-        }
-        return ParseUtil.parseLongOrDefault(split[index], -1);
     }
 }
