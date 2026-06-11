e876d1bae22203396272ee54018f407a8d37bbc3
Daniel Widdis <widdis@gmail.com>
2026-06-10 21:47:48 -0700

    Migrate DragonFly OSProcess onto BsdOSProcess
    DragonFlyBsdOSProcess extends BsdOSProcess; concretes keep the DragonFly
    specifics (/proc/<pid>/cmdline args, System.getenv, sysctl bitness,
    /proc/<pid>/status start time, group=user).

--- oshi-core-ffm/src/main/java/oshi/software/os/unix/dragonflybsd/DragonFlyBsdOSProcessFFM.java
+++ oshi-core-ffm/src/main/java/oshi/software/os/unix/dragonflybsd/DragonFlyBsdOSProcessFFM.java
@@ -11,26 +11,14 @@ import static java.lang.foreign.ValueLayout.JAVA_LONG;
 import static oshi.ffm.ForeignFunctions.callInArenaIntOrDefault;
 import static oshi.ffm.ForeignFunctions.callInArenaOrDefault;
 import static oshi.software.os.OSProcess.State.INVALID;
-import static oshi.software.os.OSProcess.State.OTHER;
-import static oshi.software.os.OSProcess.State.RUNNING;
-import static oshi.software.os.OSProcess.State.SLEEPING;
-import static oshi.software.os.OSProcess.State.STOPPED;
-import static oshi.software.os.OSProcess.State.WAITING;
-import static oshi.software.os.OSProcess.State.ZOMBIE;
 import static oshi.software.os.OSThread.ThreadFiltering.VALID_THREAD;
-import static oshi.util.Memoizer.memoize;
 
 import java.lang.foreign.MemorySegment;
 import java.nio.charset.StandardCharsets;
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.Collections;
 import java.util.List;
-import java.util.Locale;
 import java.util.Map;
-import java.util.Optional;
 import java.util.function.Predicate;
-import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
@@ -58,35 +46,6 @@ public class DragonFlyBsdOSProcessFFM extends DragonFlyBsdOSProcess {
 
     private final DragonFlyBsdOperatingSystemFFM os;
 
-    private Supplier<Integer> bitness = memoize(this::queryBitness);
-    private Supplier<String> commandLine = memoize(this::queryCommandLine);
-    private Supplier<List<String>> arguments = memoize(this::queryArguments);
-    private Supplier<Map<String, String>> environmentVariables = memoize(this::queryEnvironmentVariables);
-
-    private String name;
-    private String path = "";
-    private String user;
-    private String userID;
-    private String group;
-    private String groupID;
-    private State state = INVALID;
-    private int parentProcessID;
-    private int threadCount;
-    private int priority;
-    private long virtualSize;
-    private long residentSetSize;
-    private long kernelTime;
-    private long userTime;
-    private long startTime;
-    private long upTime;
-    private long bytesRead;
-    private long bytesWritten;
-    private long minorFaults;
-    private long majorFaults;
-    private long voluntaryContextSwitches;
-    private long involuntaryContextSwitches;
-    private String commandLineBackup;
-
     public DragonFlyBsdOSProcessFFM(int pid, Map<PsKeywords, String> psMap, DragonFlyBsdOperatingSystemFFM os) {
         super(pid);
         this.os = os;
@@ -94,31 +53,7 @@ public class DragonFlyBsdOSProcessFFM extends DragonFlyBsdOSProcess {
     }
 
     @Override
-    public String getName() {
-        return this.name;
-    }
-
-    @Override
-    public String getPath() {
-        return this.path;
-    }
-
-    @Override
-    public String getCommandLine() {
-        return this.commandLine.get();
-    }
-
-    private String queryCommandLine() {
-        String cl = String.join(" ", getArguments());
-        return cl.isEmpty() ? this.commandLineBackup : cl;
-    }
-
-    @Override
-    public List<String> getArguments() {
-        return arguments.get();
-    }
-
-    private List<String> queryArguments() {
+    protected List<String> queryArguments() {
         // DragonFlyBSD provides command line via /proc filesystem
         byte[] cmdBytes = FileUtil.readAllBytes("/proc/" + getProcessID() + "/cmdline", false);
         if (cmdBytes != null && cmdBytes.length > 0) {
@@ -128,11 +63,7 @@ public class DragonFlyBsdOSProcessFFM extends DragonFlyBsdOSProcess {
     }
 
     @Override
-    public Map<String, String> getEnvironmentVariables() {
-        return environmentVariables.get();
-    }
-
-    private Map<String, String> queryEnvironmentVariables() {
+    protected Map<String, String> queryEnvironmentVariables() {
         // DragonFlyBSD's /proc does not expose environ for other processes.
         // For the current process, use Java's System.getenv().
         int self = callInArenaIntOrDefault(arena -> FreeBsdLibcFunctions.getpid(), LOG, Level.WARN, "getpid failed",
@@ -146,86 +77,6 @@ public class DragonFlyBsdOSProcessFFM extends DragonFlyBsdOSProcess {
     @Override
     public String getCurrentWorkingDirectory() {
         return ProcstatUtil.getCwd(getProcessID());
-    }
-
-    @Override
-    public String getUser() {
-        return this.user;
-    }
-
-    @Override
-    public String getUserID() {
-        return this.userID;
-    }
-
-    @Override
-    public String getGroup() {
-        return this.group;
-    }
-
-    @Override
-    public String getGroupID() {
-        return this.groupID;
-    }
-
-    @Override
-    public State getState() {
-        return this.state;
-    }
-
-    @Override
-    public int getParentProcessID() {
-        return this.parentProcessID;
-    }
-
-    @Override
-    public int getThreadCount() {
-        return this.threadCount;
-    }
-
-    @Override
-    public int getPriority() {
-        return this.priority;
-    }
-
-    @Override
-    public long getVirtualSize() {
-        return this.virtualSize;
-    }
-
-    @Override
-    public long getResidentMemory() {
-        return this.residentSetSize;
-    }
-
-    @Override
-    public long getKernelTime() {
-        return this.kernelTime;
-    }
-
-    @Override
-    public long getUserTime() {
-        return this.userTime;
-    }
-
-    @Override
-    public long getUpTime() {
-        return this.upTime;
-    }
-
-    @Override
-    public long getStartTime() {
-        return this.startTime;
-    }
-
-    @Override
-    public long getBytesRead() {
-        return this.bytesRead;
-    }
-
-    @Override
-    public long getBytesWritten() {
-        return this.bytesWritten;
     }
 
     @Override
@@ -260,33 +111,7 @@ public class DragonFlyBsdOSProcessFFM extends DragonFlyBsdOSProcess {
     }
 
     @Override
-    public int getBitness() {
-        return this.bitness.get();
-    }
-
-    @Override
-    public long getAffinityMask() {
-        long bitMask = 0L;
-        // Would prefer to use native cpuset_getaffinity call but variable sizing is
-        // kernel-dependent and requires C macros, so we use commandline instead.
-        String cpuset = ExecutingCommand.getFirstAnswer("cpuset -gp " + getProcessID());
-        // Sample output:
-        // pid 8 mask: 0, 1
-        // cpuset: getaffinity: No such process
-        String[] split = cpuset.split(":");
-        if (split.length > 1) {
-            String[] bits = split[1].split(",");
-            for (String bit : bits) {
-                int bitToSet = ParseUtil.parseIntOrDefault(bit.trim(), -1);
-                if (bitToSet >= 0 && bitToSet < Long.SIZE) {
-                    bitMask |= 1L << bitToSet;
-                }
-            }
-        }
-        return bitMask;
-    }
-
-    private int queryBitness() {
+    protected int queryBitness() {
         return callInArenaIntOrDefault(arena -> {
             // CTL_KERN.KERN_PROC.KERN_PROC_SV_NAME.<pid>
             MemorySegment mib = arena.allocateFrom(JAVA_INT, 1, 14, 9, getProcessID());
@@ -323,26 +148,6 @@ public class DragonFlyBsdOSProcessFFM extends DragonFlyBsdOSProcess {
     }
 
     @Override
-    public long getMinorFaults() {
-        return this.minorFaults;
-    }
-
-    @Override
-    public long getMajorFaults() {
-        return this.majorFaults;
-    }
-
-    @Override
-    public long getVoluntaryContextSwitches() {
-        return this.voluntaryContextSwitches;
-    }
-
-    @Override
-    public long getInvoluntaryContextSwitches() {
-        return this.involuntaryContextSwitches;
-    }
-
-    @Override
     public boolean updateAttributes() {
         String psCommand = "ps -awwxo " + DragonFlyBsdOperatingSystemFFM.PS_COMMAND_ARGS + " -p " + getProcessID();
         List<String> procList = ExecutingCommand.runNative(psCommand);
@@ -360,29 +165,7 @@ public class DragonFlyBsdOSProcessFFM extends DragonFlyBsdOSProcess {
 
     private boolean updateAttributes(Map<PsKeywords, String> psMap) {
         long now = System.currentTimeMillis();
-        switch (psMap.get(PsKeywords.STATE).charAt(0)) {
-            case 'R':
-                this.state = RUNNING;
-                break;
-            case 'I':
-            case 'S':
-                this.state = SLEEPING;
-                break;
-            case 'D':
-            case 'L':
-            case 'U':
-                this.state = WAITING;
-                break;
-            case 'Z':
-                this.state = ZOMBIE;
-                break;
-            case 'T':
-                this.state = STOPPED;
-                break;
-            default:
-                this.state = OTHER;
-                break;
-        }
+        this.state = getStateFromOutput(psMap.get(PsKeywords.STATE).charAt(0));
         this.parentProcessID = ParseUtil.parseIntOrDefault(psMap.get(PsKeywords.PPID), 0);
         this.user = psMap.get(PsKeywords.USER);
         this.userID = psMap.get(PsKeywords.UID);
@@ -424,25 +207,5 @@ public class DragonFlyBsdOSProcessFFM extends DragonFlyBsdOSProcess {
             }
         }
         return System.currentTimeMillis();
-    }
-
-    private long getProcessOpenFileLimit(long processId, int index) {
-        final String limitsPath = String.format(Locale.ROOT, "/proc/%d/limits", processId);
-        if (!Files.exists(Paths.get(limitsPath))) {
-            return -1; // not supported
-        }
-        final List<String> lines = FileUtil.readFile(limitsPath);
-        final Optional<String> maxOpenFilesLine = lines.stream().filter(line -> line.startsWith("Max open files"))
-                .findFirst();
-        if (!maxOpenFilesLine.isPresent()) {
-            return -1;
-        }
-
-        // Split all non-Digits away -> ["", "{soft-limit}, "{hard-limit}"]
-        final String[] split = maxOpenFilesLine.get().split("\\D+");
-        if (split.length <= index) {
-            return -1;
-        }
-        return ParseUtil.parseLongOrDefault(split[index], -1);
     }
 }
--- oshi-core/src/main/java/oshi/software/os/unix/dragonflybsd/DragonFlyBsdOSProcessJNA.java
+++ oshi-core/src/main/java/oshi/software/os/unix/dragonflybsd/DragonFlyBsdOSProcessJNA.java
@@ -5,24 +5,12 @@
 package oshi.software.os.unix.dragonflybsd;
 
 import static oshi.software.os.OSProcess.State.INVALID;
-import static oshi.software.os.OSProcess.State.OTHER;
-import static oshi.software.os.OSProcess.State.RUNNING;
-import static oshi.software.os.OSProcess.State.SLEEPING;
-import static oshi.software.os.OSProcess.State.STOPPED;
-import static oshi.software.os.OSProcess.State.WAITING;
-import static oshi.software.os.OSProcess.State.ZOMBIE;
 import static oshi.software.os.OSThread.ThreadFiltering.VALID_THREAD;
-import static oshi.util.Memoizer.memoize;
 
-import java.nio.file.Files;
-import java.nio.file.Paths;
 import java.util.Collections;
 import java.util.List;
-import java.util.Locale;
 import java.util.Map;
-import java.util.Optional;
 import java.util.function.Predicate;
-import java.util.function.Supplier;
 import java.util.stream.Collectors;
 
 import org.slf4j.Logger;
@@ -54,35 +42,6 @@ public class DragonFlyBsdOSProcessJNA extends DragonFlyBsdOSProcess {
 
     private final DragonFlyBsdOperatingSystemJNA os;
 
-    private Supplier<Integer> bitness = memoize(this::queryBitness);
-    private Supplier<String> commandLine = memoize(this::queryCommandLine);
-    private Supplier<List<String>> arguments = memoize(this::queryArguments);
-    private Supplier<Map<String, String>> environmentVariables = memoize(this::queryEnvironmentVariables);
-
-    private String name;
-    private String path = "";
-    private String user;
-    private String userID;
-    private String group;
-    private String groupID;
-    private State state = INVALID;
-    private int parentProcessID;
-    private int threadCount;
-    private int priority;
-    private long virtualSize;
-    private long residentSetSize;
-    private long kernelTime;
-    private long userTime;
-    private long startTime;
-    private long upTime;
-    private long bytesRead;
-    private long bytesWritten;
-    private long minorFaults;
-    private long majorFaults;
-    private long voluntaryContextSwitches;
-    private long involuntaryContextSwitches;
-    private String commandLineBackup;
-
     public DragonFlyBsdOSProcessJNA(int pid, Map<PsKeywords, String> psMap, DragonFlyBsdOperatingSystemJNA os) {
         super(pid);
         this.os = os;
@@ -90,31 +49,7 @@ public class DragonFlyBsdOSProcessJNA extends DragonFlyBsdOSProcess {
     }
 
     @Override
-    public String getName() {
-        return this.name;
-    }
-
-    @Override
-    public String getPath() {
-        return this.path;
-    }
-
-    @Override
-    public String getCommandLine() {
-        return this.commandLine.get();
-    }
-
-    private String queryCommandLine() {
-        String cl = String.join(" ", getArguments());
-        return cl.isEmpty() ? this.commandLineBackup : cl;
-    }
-
-    @Override
-    public List<String> getArguments() {
-        return arguments.get();
-    }
-
-    private List<String> queryArguments() {
+    protected List<String> queryArguments() {
         // DragonFlyBSD provides command line via /proc filesystem
         byte[] cmdBytes = FileUtil.readAllBytes("/proc/" + getProcessID() + "/cmdline", false);
         if (cmdBytes != null && cmdBytes.length > 0) {
@@ -124,11 +59,7 @@ public class DragonFlyBsdOSProcessJNA extends DragonFlyBsdOSProcess {
     }
 
     @Override
-    public Map<String, String> getEnvironmentVariables() {
-        return environmentVariables.get();
-    }
-
-    private Map<String, String> queryEnvironmentVariables() {
+    protected Map<String, String> queryEnvironmentVariables() {
         // DragonFlyBSD's /proc does not expose environ for other processes.
         // For the current process, use Java's System.getenv().
         if (getProcessID() == DragonFlyBsdLibc.INSTANCE.getpid()) {
@@ -140,86 +71,6 @@ public class DragonFlyBsdOSProcessJNA extends DragonFlyBsdOSProcess {
     @Override
     public String getCurrentWorkingDirectory() {
         return ProcstatUtil.getCwd(getProcessID());
-    }
-
-    @Override
-    public String getUser() {
-        return this.user;
-    }
-
-    @Override
-    public String getUserID() {
-        return this.userID;
-    }
-
-    @Override
-    public String getGroup() {
-        return this.group;
-    }
-
-    @Override
-    public String getGroupID() {
-        return this.groupID;
-    }
-
-    @Override
-    public State getState() {
-        return this.state;
-    }
-
-    @Override
-    public int getParentProcessID() {
-        return this.parentProcessID;
-    }
-
-    @Override
-    public int getThreadCount() {
-        return this.threadCount;
-    }
-
-    @Override
-    public int getPriority() {
-        return this.priority;
-    }
-
-    @Override
-    public long getVirtualSize() {
-        return this.virtualSize;
-    }
-
-    @Override
-    public long getResidentMemory() {
-        return this.residentSetSize;
-    }
-
-    @Override
-    public long getKernelTime() {
-        return this.kernelTime;
-    }
-
-    @Override
-    public long getUserTime() {
-        return this.userTime;
-    }
-
-    @Override
-    public long getUpTime() {
-        return this.upTime;
-    }
-
-    @Override
-    public long getStartTime() {
-        return this.startTime;
-    }
-
-    @Override
-    public long getBytesRead() {
-        return this.bytesRead;
-    }
-
-    @Override
-    public long getBytesWritten() {
-        return this.bytesWritten;
     }
 
     @Override
@@ -250,33 +101,7 @@ public class DragonFlyBsdOSProcessJNA extends DragonFlyBsdOSProcess {
     }
 
     @Override
-    public int getBitness() {
-        return this.bitness.get();
-    }
-
-    @Override
-    public long getAffinityMask() {
-        long bitMask = 0L;
-        // Would prefer to use native cpuset_getaffinity call but variable sizing is
-        // kernel-dependent and requires C macros, so we use commandline instead.
-        String cpuset = ExecutingCommand.getFirstAnswer("cpuset -gp " + getProcessID());
-        // Sample output:
-        // pid 8 mask: 0, 1
-        // cpuset: getaffinity: No such process
-        String[] split = cpuset.split(":");
-        if (split.length > 1) {
-            String[] bits = split[1].split(",");
-            for (String bit : bits) {
-                int bitToSet = ParseUtil.parseIntOrDefault(bit.trim(), -1);
-                if (bitToSet >= 0 && bitToSet < Long.SIZE) {
-                    bitMask |= 1L << bitToSet;
-                }
-            }
-        }
-        return bitMask;
-    }
-
-    private int queryBitness() {
+    protected int queryBitness() {
         // Get process abi vector
         int[] mib = new int[4];
         mib[0] = 1; // CTL_KERN
@@ -312,26 +137,6 @@ public class DragonFlyBsdOSProcessJNA extends DragonFlyBsdOSProcess {
     }
 
     @Override
-    public long getMinorFaults() {
-        return this.minorFaults;
-    }
-
-    @Override
-    public long getMajorFaults() {
-        return this.majorFaults;
-    }
-
-    @Override
-    public long getVoluntaryContextSwitches() {
-        return this.voluntaryContextSwitches;
-    }
-
-    @Override
-    public long getInvoluntaryContextSwitches() {
-        return this.involuntaryContextSwitches;
-    }
-
-    @Override
     public boolean updateAttributes() {
         String psCommand = "ps -awwxo " + DragonFlyBsdOperatingSystemJNA.PS_COMMAND_ARGS + " -p " + getProcessID();
         List<String> procList = ExecutingCommand.runNative(psCommand);
@@ -349,29 +154,7 @@ public class DragonFlyBsdOSProcessJNA extends DragonFlyBsdOSProcess {
 
     private boolean updateAttributes(Map<PsKeywords, String> psMap) {
         long now = System.currentTimeMillis();
-        switch (psMap.get(PsKeywords.STATE).charAt(0)) {
-            case 'R':
-                this.state = RUNNING;
-                break;
-            case 'I':
-            case 'S':
-                this.state = SLEEPING;
-                break;
-            case 'D':
-            case 'L':
-            case 'U':
-                this.state = WAITING;
-                break;
-            case 'Z':
-                this.state = ZOMBIE;
-                break;
-            case 'T':
-                this.state = STOPPED;
-                break;
-            default:
-                this.state = OTHER;
-                break;
-        }
+        this.state = getStateFromOutput(psMap.get(PsKeywords.STATE).charAt(0));
         this.parentProcessID = ParseUtil.parseIntOrDefault(psMap.get(PsKeywords.PPID), 0);
         this.user = psMap.get(PsKeywords.USER);
         this.userID = psMap.get(PsKeywords.UID);
@@ -413,25 +196,5 @@ public class DragonFlyBsdOSProcessJNA extends DragonFlyBsdOSProcess {
             }
         }
         return System.currentTimeMillis();
-    }
-
-    private long getProcessOpenFileLimit(long processId, int index) {
-        final String limitsPath = String.format(Locale.ROOT, "/proc/%d/limits", processId);
-        if (!Files.exists(Paths.get(limitsPath))) {
-            return -1; // not supported
-        }
-        final List<String> lines = FileUtil.readFile(limitsPath);
-        final Optional<String> maxOpenFilesLine = lines.stream().filter(line -> line.startsWith("Max open files"))
-                .findFirst();
-        if (!maxOpenFilesLine.isPresent()) {
-            return -1;
-        }
-
-        // Split all non-Digits away -> ["", "{soft-limit}, "{hard-limit}"]
-        final String[] split = maxOpenFilesLine.get().split("\\D+");
-        if (split.length <= index) {
-            return -1;
-        }
-        return ParseUtil.parseLongOrDefault(split[index], -1);
     }
 }
